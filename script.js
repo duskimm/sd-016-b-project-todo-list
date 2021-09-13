f269d5821ccbb161a66af30b782b47eaec2e2fda
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-13 16:53:18 -0300

    Requirements 7 and 8

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+function selectTask(event) {
+  const lis = document.getElementsByTagName('li');
+  const task = event.target;
+  for (let i = 0; i < lis.length; i += 1) {
+    lis[i].classList.remove('selected');
+  }
+  task.classList.add('selected');
+}
+
 function addTask(list) {
   const input = document.querySelector('#texto-tarefa');
   const task = document.createElement('li');
+  task.addEventListener('click', selectTask);
   task.innerText = input.value;
   input.value = '';
   list.appendChild(task);
