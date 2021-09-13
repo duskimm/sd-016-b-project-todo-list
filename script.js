a5fce691aae0c5b1fe14dd7ddaa63ae93447b7b8
gab <gabrielfontes321@gmail.com>
2021-09-13 18:56:04 -0300

    Requisito 11, lint e lint:style completos

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-var tasksList = document.getElementById('lista-tarefas');
-var addButton = document.getElementById('criar-tarefa');
-var removeButton = document.getElementById('apaga-tudo')
-var inputt = document.getElementById('texto-tarefa');
-var tasks = document.getElementsByClassName('tasks');
-var li = document.getElementsByTagName('li')
+const tasksList = document.getElementById('lista-tarefas');
+const addButton = document.getElementById('criar-tarefa');
+const removeButton = document.getElementById('apaga-tudo');
+const inputt = document.getElementById('texto-tarefa');
+const tasks = document.getElementsByClassName('tasks');
+const finishedButton = document.getElementById('remover-finalizados');
 
-addButton.addEventListener('click', function addTasksList(){
-  var elementLi = document.createElement('li');
-  let text = inputt.value;
+addButton.addEventListener('click', () => {
+  const elementLi = document.createElement('li');
+  const text = inputt.value;
   elementLi.className = 'tasks';
   tasksList.appendChild(elementLi);
   elementLi.innerText = text;
   inputt.value = '';
-}) 
+});
 
-tasksList.addEventListener('click', function(event){
+tasksList.addEventListener('click', (event) => {
+  const eventt = event.target;
   for (let i = 0; i < tasks.length; i += 1) {
     tasks[i].style.backgroundColor = '';
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-})
+  eventt.style.backgroundColor = 'rgb(128, 128, 128)';
+});
 
-tasksList.addEventListener('dblclick', function(event){
-  event.target.classList.add('completed');
-})
-
-removeButton.addEventListener('click', function(){
-  for (let i = 0; i < tasks.length; i = 0) {
-    tasksList.removeChild(tasks[i]) 
+tasksList.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
   }
-})
+});
 
+removeButton.addEventListener('click', () => {
+  for (let i = 0; i < tasks.length; i = 0) {
+    tasksList.removeChild(tasks[i]);
+  }
+// Créditos ao Léo.
+});
+
+finishedButton.addEventListener('click', () => {
+  while (document.querySelector('.completed')) {
+    document.querySelector('.completed').remove();
+  }
+// Créditos ao Gabriel Rodrigues Leite.
+});
