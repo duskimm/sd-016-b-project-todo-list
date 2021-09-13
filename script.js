76e4a556490a88df14789f067c0bacb5f9d75830
Diesel <jpedro.diesel@gmail.com>
2021-09-13 20:00:04 -0300

    Requisito 10 feito

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+//Requisito 5
 let newTask = document.getElementById('texto-tarefa');
 let taskList = document.getElementById('lista-tarefas');
 let createNewTask = document.getElementById('criar-tarefa');
+let taskToSelect = document.querySelector('li');
+let buttonDeleteAll = document.getElementById('apaga-tudo')
 
-function addNewTask () {
-  let taskListItem = document.createElement('li');
+function addNewTask() {
+  const taskListItem = document.createElement('li');
   taskList.appendChild(taskListItem);
   taskListItem.innerText = newTask.value;
-  newTask.value = ''
+  newTask.value = '';
+}
+createNewTask.addEventListener('click', addNewTask);
+
+//Requisito 5 feito graças ao link 
+//https://stackoverflow.com/questions/17433557/how-to-save-user-input-into-a-variable-in-html-and-javascript
+ 
+//Requisito 6
+function markTask() {
+  taskToSelect.style.backgroundColor  = "rgb(128, 128, 128)";
 }
 
-createNewTask.addEventListener('click', addNewTask);
+taskList.addEventListener('click',markTask);
+
+function deleteAll() {
+  taskList.innerHTML = ""     
+}
+
+buttonDeleteAll.addEventListener('click',deleteAll);
