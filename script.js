2c277fcfff7cb29113c6437ab49d9b4f72eef3dc
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-13 17:07:29 -0300

    Challenge 5 done

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function toDoCreation () {
-  
+const toDoSend = document.querySelector('#criar-tarefa');
+const appendList = document.querySelector('#lista-tarefas');
+const getToDoValue = document.querySelector('#texto-tarefa');
+
+function toDoCreation() {
+  if (getToDoValue.value !== '') {
+    let createElementList = document.createElement('li');
+    createElementList.innerHTML = getToDoValue.value;
+    appendList.appendChild(createElementList);
+    getToDoValue.value = '';
+  } else {
+    alert('Informar uma tarefa');
+  }
 }
+toDoSend.addEventListener('click', toDoCreation);
