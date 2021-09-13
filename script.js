433950801fa976276d6ef744f09823e640bba633
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-13 16:49:50 -0300

    Conclui requisito 8

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ button.addEventListener('click', function () {
 // Selecionado tarefa da lista
 orderedList.addEventListener('click', function (event) {
   if (event.target.id !== 'lista-tarefas') {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    let listItems = orderedList.children;
+    let currentSelection = event.target;
+    for (item of listItems) {
+      item.style.backgroundColor = ''
+    }
+    currentSelection.style.backgroundColor = 'rgb(128, 128, 128)';
   }
 });
