0d8e391832c908044632556ebc586819957e42c7
lucas <araujo2012@hotmail.com>
2021-09-13 14:39:12 -0300

    Requisito 5 e 6 completo

---
+++ script.js
@@ -0,0 +1,21 @@
+// Funcao para criar elementos fora do dom, recebe como parametros respectivamente o tipo do elemento, o texto, as classes, e o id, se passar '' como parametro ele nao adiciona o parametro em questao.
+function newChild(type, text, classs, id) {
+	const newChildItem = document.createElement(type);
+	newChildItem.innerText = text;
+	if (classs !== '') {
+	  newChildItem.className = classs;
+	}
+	if (id !== '') {
+	  newChildItem.id = id;
+	}
+	return newChildItem;
+}
+
+document.querySelector('#criar-tarefa').addEventListener('click', () => {
+	const inputText = document.querySelector('#texto-tarefa');
+	const todoList = document.querySelector('#lista-tarefas');
+	if (inputText !== '') {
+		todoList.append(newChild('li', inputText.value, '', ''));
+		inputText.value = '';
+	}
+});
