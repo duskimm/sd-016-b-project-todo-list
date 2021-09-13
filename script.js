7812e30c275917ac76470ca61de25643fdb060a5
Matheus <matheusmoura2110@gmail.com>
2021-09-13 15:31:17 -0300

    adiciona a cor cinza nos elementos clicados

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function addtask() {
   document.getElementById('lista-tarefas').innerHTML = list;
   document.getElementById('texto-tarefa').value = null;
 }
+
 //indicação para tirar ideia do video: https://www.youtube.com/watch?v=pRwxgtqImZQ
+
+const toGray = document.getElementById('lista-tarefas');
+function changeColor(event) {
+  const selectedItem = document.querySelectorAll('li');
+  for (let index = 0; index < selectedItem.length; index += 1) {
+    selectedItem[index].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+toGray.addEventListener('click', changeColor);
