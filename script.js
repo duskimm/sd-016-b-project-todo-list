62ee23d2da9fe760fe77dce65703ff984117738d
Eglany Junior <contas.eglanyjr@gmail.com>
2021-09-13 16:30:38 -0400

    selecionar li e class completed com dblclick(bug)

--- script.js
+++ script.js
@@ -5,9 +5,36 @@ const todoList = document.querySelector('#lista-tarefas')
 
 function createTodo() {
   const createLi = document.createElement('li');
-	let text = todoText.value;
-	todoList.appendChild(createLi).innerText = text;
+  let text = todoText.value;
+  todoList.appendChild(createLi).innerText = text;
 
 }
 
 createButton.addEventListener('click', createTodo);
+
+//selecionar li
+let liTarget = '';
+
+function selectedLi(event){
+  unselecteLi();
+  liTarget = event.target
+  liTarget.className = 'selected'
+}
+
+function unselecteLi(){
+  if (liTarget.className === 'selected'){
+    liTarget.classList.remove('selected');
+  }
+}
+todoList.addEventListener('click', selectedLi);
+
+//tarela completa ---- resolver, mais de uma selecao 
+function completTask(event) {
+  if (liTarget.className === 'selected') {
+    liTarget.classList = 'selected completed';
+    unselecteLi();
+    console.log(event.target);
+  }
+}
+
+todoList.addEventListener('dblclick', completTask);
