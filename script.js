bfaedc7ccc96c48d078c38b17d9a0f8e7e4aa813
elton <eltonmorenocl@gmail.com>
2021-09-13 15:18:19 -0300

    requisito 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Criar header
+function createHeader() {
+  const newHeader = document.createElement('header');
+  newHeader.innerHTML = 'Minha Lista de Tarefas';
+  document.body.appendChild(newHeader);
+}
+createHeader();
