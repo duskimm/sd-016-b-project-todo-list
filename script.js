03e438adbf1e956084eea824f30fce64ed269e31
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-13 17:00:22 -0300

    até o requisito 10 tudo ok

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// console.log(document.querySelector('button'))
-
 function addTarefa() {
   if (document.querySelector('#texto-tarefa').value.length > 0) {
-    // console.log(document.querySelector('#texto-tarefa').value);
     let li = document.createElement('li');
     li.innerText = document.querySelector('#texto-tarefa').value;
+    document.querySelector('#texto-tarefa').value = '';
+    li.classList.add('tarefa');
     li.onclick = selecionar;
+    li.ondblclick = riscar;
     document.querySelector('#lista-tarefas').appendChild(li);
   }
 }
 
-//  Habilita seleção
+//  5-8 Habilita seleção
 function selecionar(e) {
   if (document.querySelector('.selecionado')) {
     document.querySelector('.selecionado').classList.remove('selecionado');
@@ -18,9 +18,26 @@ function selecionar(e) {
   e.target.classList.add('selecionado');
 }
 
+//  9 Habilita riscar tarefa
+function riscar(e) {
+  if ( e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+  } else {
+    e.target.classList.add('completed');
+  }
+}
+
+//  10 Habilita apagar tudo
+function limparLista() {
+  while (document.querySelector('.tarefa')) {
+    document.querySelector('.tarefa').remove();
+  }
+}
+
 //  Configurar a página
 function configurar() {
   document.querySelector('#criar-tarefa').addEventListener('click', addTarefa);
+  document.querySelector('#apaga-tudo').addEventListener('click', limparLista);
 }
 
 window.onload = configurar;
