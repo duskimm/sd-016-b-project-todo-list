348631d2ef11dbd03d133dbc9bc874b06e9d7cbb
Matheus <matheusmoura2110@gmail.com>
2021-09-13 16:19:16 -0300

    adiciona estilo de riscar linha com dbclick e fundo gradiente no css

--- script.js
+++ script.js
@@ -11,10 +11,21 @@ function addtask() {
 
 const toGray = document.getElementById('lista-tarefas');
 function changeColor(event) {
-  const selectedItem = document.querySelectorAll('li');
+  const selectedItem = document.querySelectorAll('li')
   for (let index = 0; index < selectedItem.length; index += 1) {
-    selectedItem[index].style.backgroundColor = '';
+    selectedItem[index].style.backgroundColor = ""
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 toGray.addEventListener('click', changeColor);
+
+const clearTab = document.getElementById('apaga tudo')
+
+function riscaItem(event) {
+  if (event.target.className === "completed") {
+    event.target.className = ""
+  } else {
+    event.target.className = "completed"
+  }
+}
+toGray.addEventListener("dblclick", riscaItem);
