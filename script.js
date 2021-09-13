913473fc260796659f78f287029012b64043f83a
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-13 18:12:11 -0300

    Challenge 7 done

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const appendList = document.querySelector('#lista-tarefas');
 const getToDoValue = document.querySelector('#texto-tarefa');
 
 function toDoCreation() {
-  if (getToDoValue.value !== '') {
-    let createElementList = document.createElement('li');
-    createElementList.innerHTML = getToDoValue.value;
-    appendList.appendChild(createElementList);
-    getToDoValue.value = '';
-  } else {
-    alert('Informar uma tarefa');
-  }
+  let createElementList = document.createElement('li');
+  createElementList.innerHTML = getToDoValue.value;
+  createElementList.classList = 'task';
+  appendList.appendChild(createElementList);
+  getToDoValue.value = '';
 }
 toDoSend.addEventListener('click', toDoCreation);
+
+function backgroundToDoList() {
+  const taskColor = document.querySelectorAll('li');
+
+  for (let color of taskColor) {
+    color.style.backgroundColor = 'rgb(194, 194, 194)';
+    } 
+    appendList.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+}
+
+backgroundToDoList();
