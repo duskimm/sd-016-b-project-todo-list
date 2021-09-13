cc2e7c2a932b68fe4e634b069344b3aea58dc77f
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-13 15:59:17 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 window.onload = function () {
+  function header() {
+    const headerTitle = document.getElementById('header-container');
+    const headerH1 = document.createElement('h1');
+    headerH1.innerText = 'Minha Lista de Tarefas';
+    headerH1.id = 'title';
+    headerTitle.appendChild(headerH1);
+  }
+  header();
+
+  function textParagraph() {
+    const headerTitle = document.getElementById('header-container');
+    const paragraph = document.createElement('p');
+    paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+    paragraph.id = 'funcionamento';
+    headerTitle.appendChild(paragraph);
+  }
+  textParagraph();
+
+  function assignmentInput() {
+    const assignment = document.getElementById('header-container');
+    const assignmentText = document.createElement('input');
+    assignmentText.id = 'texto-tarefa';
+    assignment.appendChild(assignmentText);
+  }
+  assignmentInput();
 }
 
-function header() {
-  const headerTitle = document.getElementById('header-container');
-  const headerH1 = document.createElement('h1');
-  headerH1.innerText ='Minha Lista de Tarefas';
-  headerTitle.appendChild(headerH1);
-}
-header();
