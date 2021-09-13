63e4937c381ff544ef9c8abe6942d52af791642e
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-13 15:15:03 -0300

    Requisito 4 feito

---
+++ script.js
@@ -0,0 +1,4 @@
+let inputElement = document.querySelector('#texto-tarefa');
+let listOrdenada = document.createElement('ol');
+listOrdenada.id = 'lista-tarefas';
+inputElement.appendChild(listOrdenada);
