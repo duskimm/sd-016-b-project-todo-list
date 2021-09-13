be25f98c7ea3b2d1d296bb4aa61f273fa5826525
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-13 15:39:43 -0300

    requisito -09 realizado

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addTask() {
     if (txtInput.value !== '') {
       const listElement = document.createElement('li');
       listElement.innerHTML = txtInput.value;
+      listElement.classList.add('taskDone');
       toDoList.appendChild(listElement);
       txtInput.value = '';
       txtInput.focus();
@@ -20,8 +21,22 @@ addTask();
 
 function selectTask() {
   toDoList.addEventListener('click', (event) => {
+    const taskSelected = document.querySelectorAll('.selected');
+    for (let i = 0; i < taskSelected.length; i++) {
+      taskSelected[i].classList.remove('selected');
+    }
     event.target.classList.toggle('selected');
   });
 }
 
 selectTask();
+
+function taskDone() {
+  toDoList.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('taskDone')) {
+      event.target.classList.toggle('completed');
+    }
+  });
+}
+
+taskDone();
