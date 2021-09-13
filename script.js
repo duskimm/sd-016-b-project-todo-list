7766a4b140c9bbc8a30ded78d6855a50ca9c421c
Rael N. Pires <rnogpiresfinancial@gmail.com>
2021-09-13 18:48:20 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-//const taskInput = document.querySelector('#texto-tarefa');
-
-//const taskString = taskInput.value;
-
 const list = document.getElementById('lista-tarefas');
 
 const button = document.querySelector('#criar-tarefa');
 
-function addTaskToList (event) {
+function addTaskToList(event) {
   event.preventDefault();
 
   const taskInput = document.getElementById('texto-tarefa');
   const taskString = taskInput.value;
   const newTask = document.createElement('li');
 
+  newTask.className = 'listItem';
+
   newTask.innerText = taskString;
   list.appendChild(newTask);
   taskInput.value = '';
-};
+}
 
 button.addEventListener('click', addTaskToList);
+
+function selectItem(event) {
+  const selectedTask = event.target;
+
+  selectedTask.classList.add('selected');
+}
+
+list.addEventListener('click', selectItem);
