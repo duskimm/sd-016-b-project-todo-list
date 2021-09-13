cd0ce8bee7af8958ce079ea7c0fc55959467e2be
victor_calidor <victor.calidor@gmail.com>
2021-09-13 18:30:03 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const button = document.getElementById('criar-tarefa');
+const button1 = document.getElementById('criar-tarefa');
+const button2 = document.getElementById('apaga-tudo');
 const lista = document.getElementById('lista-tarefas');
 
 /// Mudar cor da tarefa selecionada
@@ -12,7 +13,17 @@ function mudarCor(event) {
   }
 }
 
-/// Criar nova tarefa
+/// Riscar e desriscar tarefa completa
+
+function tarefaCompleta(event) {
+  if (event.target.className === 'completed') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.className = 'completed';
+  }
+}
+
+/// Criar nova tarefa e adicionar event listeners
 
 function criarTarefa() {
   const inputTexto = document.getElementById('texto-tarefa').value;
@@ -20,7 +31,18 @@ function criarTarefa() {
   novaTarefa.innerText = inputTexto;
   document.getElementById('texto-tarefa').value = '';
   novaTarefa.addEventListener('click', mudarCor);
+  novaTarefa.addEventListener('dblclick', tarefaCompleta);
   lista.appendChild(novaTarefa);
 }
 
-button.addEventListener('click', criarTarefa);
+button1.addEventListener('click', criarTarefa);
+
+/// Apagar todas tarefas
+function apagarTarefas() {
+  for (let i = lista.children.length; i > 0; i -= 1) {
+    const ultimaTarefa = lista.lastChild;
+    lista.removeChild(ultimaTarefa);
+  }
+}
+
+button2.addEventListener('click', apagarTarefas);
