cae61af83c914cc6fc673f8f5be1d27c332c130c
Diego-AS-Mendes <dasm021@msn.com>
2021-09-13 17:10:22 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+const doubleClickText = document.createElement('p');
+doubleClickText.setAttribute('id', 'funcionamento');
+doubleClickText.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+document.body.appendChild(doubleClickText);
