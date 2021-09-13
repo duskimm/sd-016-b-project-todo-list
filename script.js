2e0ebff44dd38dc155a77cc55033e1a128356107
Everton-Curti <evercurti@gmail.com>
2021-09-13 15:08:00 -0300

    exercicio 1 concluido

---
+++ script.js
@@ -0,0 +1,4 @@
+let capturarCabecalho = document.querySelector('#cabecalho');
+let criarCabecalho = document.createElement('h1');
+criarCabecalho.innerHTML = 'Minha Lista de Tarefas';
+capturarCabecalho.appendChild(criarCabecalho);
