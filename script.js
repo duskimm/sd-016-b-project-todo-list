cc6521c03c3475e30c33e19906737e2e210fa134
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-13 16:42:34 -0300

    seleção habilitada

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let teste = document.createElement('h1');
-teste.innerText = 'Teste js';
-document.querySelector('body').appendChild(teste);
+// console.log(document.querySelector('button'))
 
-console.log('ok');
+function addTarefa() {
+  if (document.querySelector('#texto-tarefa').value.length > 0) {
+    // console.log(document.querySelector('#texto-tarefa').value);
+    let li = document.createElement('li');
+    li.innerText = document.querySelector('#texto-tarefa').value;
+    li.onclick = selecionar;
+    document.querySelector('#lista-tarefas').appendChild(li);
+  }
+}
 
+//  Habilita seleção
+function selecionar(e) {
+  if (document.querySelector('.selecionado')) {
+    document.querySelector('.selecionado').classList.remove('selecionado');
+  }
+  e.target.classList.add('selecionado');
+}
+
+//  Configurar a página
+function configurar() {
+  document.querySelector('#criar-tarefa').addEventListener('click', addTarefa);
+}
+
+window.onload = configurar;
 
 // Linter, você está feliz?
