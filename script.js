ff160b195b14078005c54b7bb41ed848286c5492
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-13 16:12:43 -0300

    passo 4

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function inputed(){
 const bodyElement = document.body;
+function inputed(){
 const createInput = document.createElement('input');
 createInput.id ='texto-tarefa';
 bodyElement.appendChild(createInput);
 }
 inputed();
+
+function list(){
+const createListOl = document.createElement('ol');
+createListOl.id ='lista-tarefas';
+bodyElement.appendChild(createListOl);
+}
+list();
