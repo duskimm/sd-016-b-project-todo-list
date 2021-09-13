b4f7207795095de337c8aaf84f53be85371e75e2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-13 14:48:35 -0300

    creation of functions to work with DOM elements

---
+++ script.js
@@ -0,0 +1,37 @@
+// functions for working with elements
+
+function getOne(element) {
+  return document.querySelector(element);
+}
+
+function getAll(element) {
+  return document.querySelectorAll(element);
+}
+
+function createElement(tag) {
+  return document.createElement(tag);
+}
+
+function addClass(element, newClass) {
+  element.classList.add(newClass);
+}
+
+function removeClass(element, delClass) {
+  element.classList.remove(delClass);
+}
+
+function plugHtml(fatherElement, sonElement) {
+  fatherElement.appendChild(sonElement);
+}
+
+function removeOfHtml(fatherElement, sonElement) {
+  fatherElement.removeChild(sonElement);
+}
+
+function addMultiplesEvents(element, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  events.forEach((event) => {
+    element.addEventListener(event, listener, false);
+  });
+}
