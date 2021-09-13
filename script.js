6aba47e295436dd2dd03d1da71f3c36441c263a4
Victor <victor.hugofla@hotmail.com>
2021-09-13 16:52:35 -0300

    Finaliza requisito 9

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputTasks = document.getElementById('texto-tarefa');
 const taskButton = document.getElementById('criar-tarefa');
 const itens = document.getElementsByClassName('itens');
 
-function addClass(e) {
+function addClassSelected(e) {
   for (let i = 0; i < itens.length; i += 1) {
     itens[i].classList.remove('selected');
   }
@@ -12,10 +12,21 @@ function addClass(e) {
 
 function addEventLi() {
   for (let i = 0; i < itens.length; i += 1) {
-    itens[i].addEventListener('click', addClass);
+    itens[i].addEventListener('click', addClassSelected);
   }
 }
 addEventLi();
+
+function addClassCompleted(e) {
+  e.target.classList.toggle('completed');
+}
+
+function addEventLi2() {
+  for (let i = 0; i < itens.length; i += 1) {
+    itens[i].addEventListener('dblclick', addClassCompleted);
+  }
+}
+addEventLi2();
 
 function addTasks() {
   const inputValue = inputTasks.value;
@@ -25,6 +36,7 @@ function addTasks() {
   orderedList.appendChild(createLi);
   inputTasks.value = '';
   addEventLi();
+  addEventLi2();
 }
 
 taskButton.addEventListener('click', addTasks);
