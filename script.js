4f648c22e9c8202078746c304dc053f3c08d0691
Bruno Matumoto <brunomatumoto@MacBook-Air-de-Bruno.local>
2021-09-13 16:52:43 -0300

    Add exercise 7 completed to project

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const btnTask = document.getElementById('criar-tarefa');
 
+function itemTask() {
+  const tasks = document.querySelectorAll('#lista-tarefas li');
+  tasks.forEach((item) => {
+    item.addEventListener('click', () => {
+      const element = item;
+      element.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  });
+}
+
 btnTask.addEventListener('click', () => {
   const nameTask = document.getElementById('texto-tarefa').value;
   const listTasks = document.getElementById('lista-tarefas');
@@ -7,4 +17,5 @@ btnTask.addEventListener('click', () => {
   newTask.innerText = nameTask;
   listTasks.appendChild(newTask);
   document.getElementById('texto-tarefa').value = '';
+  itemTask();
 });
