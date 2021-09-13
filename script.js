447e9d070d5d52b3ec31fb27e4493f2f82ba2db8
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-09-13 15:32:04 -0300

    requisito 8 ok

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getInput = document.getElementById('texto-tarefa');
 const getButton = document.getElementById('criar-tarefa');
 const getOrderlist = document.getElementById('lista-tarefas');
+const getItemList = document.getElementsByClassName('itemList');
 
 // Adicinar valores a lista 
 getButton.addEventListener('click', function () {
@@ -11,6 +12,11 @@ getButton.addEventListener('click', function () {
   getInput.value = '';
 });
 
+
+
 getOrderlist.addEventListener('click', function(event){
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    });
+  for (let i = 0; i < getItemList.length; i += 1) {
+    getItemList[i].style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';   
+});
