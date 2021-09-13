751fda51c6bbe7c8c73c6127e7117b7028fea8fd
Diesel <jpedro.diesel@gmail.com>
2021-09-13 18:32:27 -0300

    Requisito 5 feito (DEPOIS DE 3HRS, EU TO MORRENDO)

---
+++ script.js
@@ -0,0 +1,12 @@
+let newTask = document.getElementById('texto-tarefa');
+let taskList = document.getElementById('lista-tarefas');
+let createNewTask = document.getElementById('criar-tarefa');
+
+function addNewTask () {
+  let taskListItem = document.createElement('li');
+  taskList.appendChild(taskListItem);
+  taskListItem.innerText = newTask.value;
+  newTask.value = ' '
+}
+
+createNewTask.addEventListener('click', addNewTask);
