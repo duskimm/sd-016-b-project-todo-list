c7870932a3b981c1af289b75b30062e880dc1237
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-13 16:03:01 -0300

    Requisit - 3

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('oi');
+const inputArea = document.getElementById('input-area');
+
+function inputTarefa () {
+  const input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  input.type = 'text';
+  input.placeholder = 'Adicione sua nova tarefa';
+  inputArea.appendChild(input);
+
+}
+inputTarefa();
