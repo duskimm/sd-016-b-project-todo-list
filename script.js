0f2afffd8997b7526e5153fc2668d6dba3bf7e89
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-13 16:28:33 -0300

    Requisito - 5 e 6

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const inputArea = document.getElementById('input-area');
 const listaArea = document.getElementById('lista-area');
 
-function createInputTarefa () {
+// const buttonsArea = document.getElementById('buttons-area');
+
+// Cria input
+function createInputTarefa() {
   const input = document.createElement('input');
   input.id = 'texto-tarefa';
   input.type = 'text';
   input.placeholder = 'Adicione sua nova tarefa';
   inputArea.appendChild(input);
-
 }
 createInputTarefa();
 
-function createListOrdenada () {
+// Cria lista ordenada
+function createListOrdenada() {
   const olList = document.createElement('ol');
   olList.id = 'lista-tarefas';
   listaArea.appendChild(olList);
 }
 createListOrdenada();
+
+// Cria botão criar tarefa
+function createButtonTarefa() {
+  const button = document.createElement('button');
+  button.id = 'criar-tarefa';
+  button.innerText = 'Adicionar tarefa';
+  inputArea.appendChild(button);
+}
+createButtonTarefa();
+
+const olList = document.getElementById('lista-tarefas');
+const buttonAddIten = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+buttonAddIten.addEventListener('click', () => {
+  const inputValue = document.querySelector('#texto-tarefa').value;
+  const li = document.createElement('li');
+  li.innerText = inputValue;
+  olList.appendChild(li);
+  input.value = '';
+});
