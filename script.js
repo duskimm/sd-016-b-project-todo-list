18206e99f49c630938bedada46144d3d3539e4c4
gab <gabrielfontes321@gmail.com>
2021-09-13 16:48:56 -0300

    Requisito 8, 9 e 10

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 var tasksList = document.getElementById('lista-tarefas');
 var addButton = document.getElementById('criar-tarefa');
+var removeButton = document.getElementById('apaga-tudo')
 var inputt = document.getElementById('texto-tarefa');
+var tasks = document.getElementsByClassName('tasks');
+var li = document.getElementsByTagName('li')
 
-// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão,
-// um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
 addButton.addEventListener('click', function addTasksList(){
   var elementLi = document.createElement('li');
   let text = inputt.value;
+  elementLi.className = 'tasks';
   tasksList.appendChild(elementLi);
   elementLi.innerText = text;
   inputt.value = '';
 }) 
 
 tasksList.addEventListener('click', function(event){
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 })
+
+tasksList.addEventListener('dblclick', function(event){
+  event.target.classList.add('completed');
+})
+
+removeButton.addEventListener('click', function(){
+  for (let i = 0; i < tasks.length; i = 0) {
+    tasksList.removeChild(tasks[i]) 
+  }
+})
+
