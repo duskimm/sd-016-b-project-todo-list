57146fa5ee98304e64a77dc1410d6b1189323964
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-13 16:04:37 -0300

    passo 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,7 @@
+function inputed(){
+const bodyElement = document.body;
+const createInput = document.createElement('input');
+createInput.id ='texto-tarefa';
+bodyElement.appendChild(createInput);
+}
+inputed();
