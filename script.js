7503286ee310fba6b0949ca68f83be2dc9e54796
Diego-AS-Mendes <dasm021@msn.com>
2021-09-13 18:10:55 -0300

    Requisitos 3 e  4

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const doubleClickText = document.createElement('p');
 doubleClickText.setAttribute('id', 'funcionamento');
 doubleClickText.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 document.body.appendChild(doubleClickText);
+
+const adicionaALista = document.createElement('input');
+adicionaALista.setAttribute('id', 'texto-tarefa');
+document.body.appendChild(adicionaALista);
+
+const listaDeTarefas = document.createElement('ol');
+listaDeTarefas.setAttribute('id', 'lista-tarefas');
+document.body.appendChild(listaDeTarefas);
