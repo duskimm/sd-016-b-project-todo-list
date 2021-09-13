eb4ca857ed5dc41c67e2f70590b509b6bcf104d4
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-13 16:49:06 -0300

    Requisito 5 e 6 feito

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let inputElement = document.querySelector('main');
-let listOrdenada = document.createElement('ol');
+const inputElement = document.querySelector('main');
+const listOrdenada = document.createElement('ol');
+const inputT = document.querySelector('#texto-tarefa');
 listOrdenada.id = 'lista-tarefas';
 inputElement.appendChild(listOrdenada);
+
+// Requisito 5 funcionalidade do botão
+
+const botao = document.querySelector('#criar-tarefa');
+botao.addEventListener('click', function() {
+  let text = inputT.value
+  let elementList = document.createElement('li');
+  elementList.classList.add('lista');
+  listOrdenada.appendChild(elementList);
+  elementList.innerText = text;
+  inputT.value = '';
+})
