178931c7e8ee2eac7350283a1878d25d81e7a2a6
victor_calidor <victor.calidor@gmail.com>
2021-09-13 15:48:01 -0300

    Requisito 5

---
+++ script.js
@@ -0,0 +1,13 @@
+/// Criar nova tarefa
+
+const button = document.getElementById('criar-tarefa');
+const lista = document.getElementById('lista-tarefas');
+
+function criarTarefa() {
+  const inputTexto = document.getElementById('texto-tarefa').value;
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerText = inputTexto;
+  lista.appendChild(novaTarefa);
+}
+
+button.addEventListener('click', criarTarefa);
