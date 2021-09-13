5c03f6b8b49aacf7ce22bfd345a273493a29b86e
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-13 20:19:55 -0300

    obrigatorios

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const creatTitle = document.createElement('h1');
 creatTitle.innerHTML = 'Minha Lista de Tarefas'
 divBody.appendChild(creatHeader)
 creatHeader.appendChild(creatTitle)
+const listaTarefas = JSON.parse(localStorage.getItem('list_tarefas'))
 
 function paragraph(params) {
     const creatParagraph = document.createElement('p')
@@ -14,6 +15,8 @@ function paragraph(params) {
     creatParagraph.id = 'funcionamento'
 }
 paragraph('Clique duas vezes em um item para marcá-lo como completo')
+const creatSectionButtons = document.createElement('section')
+divBody.appendChild(creatSectionButtons)
 
 function inputList() {
     const creatDivInput = document.createElement('div')
@@ -36,10 +39,9 @@ List()
 
 function buttons(id, nome) {
     const creatButtons = document.createElement('button')
-    const creatSectionButtons = document.createElement('section')
+    creatSectionButtons.id = "buttons"
     creatButtons.id = id
     creatButtons.innerHTML = nome
-    divBody.appendChild(creatSectionButtons)
     creatSectionButtons.appendChild(creatButtons)
 
 }
@@ -53,27 +55,73 @@ function creatItemList() {
         let creatLi = document.createElement('li')
         creatLi.classList.add('item')
         catchList.appendChild(creatLi)
-        creatLi.innerHTML = catchimput.value
+        let imputValue = catchimput.value
+        creatLi.innerHTML = imputValue
         catchimput.value = ''
+        listaTarefas.push(imputValue)
     })
-
+    saveList()
 }
 creatItemList()
 
-function backGColorLi() {
+function selectLi() {
     const catchOl = document.querySelector('#lista-tarefas')
-    catchOl.addEventListener('click',(event)=> {
+    catchOl.addEventListener('click', (event) => {
         removeSelect()
         event.target.classList.add('selected')
-        
+
     })
 }
-backGColorLi()
+selectLi()
 
 function removeSelect() {
     const catchLi = document.querySelectorAll('.item')
     for (let index = 0; index < catchLi.length; index += 1) {
         catchLi[index].classList.remove('selected');
     }
-  }
-  
+}
+function completedLi() {
+    const catchOl = document.querySelector('#lista-tarefas')
+    catchOl.addEventListener('dblclick', (event) => {
+        if (event.target.classList == 'item' || event.target.classList == 'item selected') {
+            event.target.classList.add('completed')
+        }
+        else {
+            event.target.classList.remove('completed')
+        }
+    })
+
+}
+completedLi()
+
+buttons('apaga-tudo', 'apagar')
+/* para remover de forma dinamica, usei essa dica https://pt.stackoverflow.com/questions/441373/como-remover-todos-os-elementos-de-uma-div-em-javascript */
+function buttonRemove() {
+    const catchOl = document.querySelector('#lista-tarefas')
+    const catchButtonRemove = document.querySelector('#apaga-tudo')
+    catchButtonRemove.addEventListener('click',() =>{
+       catchOl.innerHTML = ''
+    })
+    saveList()
+}
+buttonRemove()
+
+buttons('remover-finalizados', 'apagar finalizados')
+/* essa função while eu peguei com base no repositorio do gabriel rodrigues https://github.com/tryber/sd-016-b-project-todo-list/pull/78/commits/81c677b62f4d88614106cd4f367d971583029898 */
+function buttonRemoveCompleted() {
+    const catchButtonRemoveCompleted = document.querySelector('#remover-finalizados')
+    catchButtonRemoveCompleted.addEventListener('click',() =>{
+        while (document.querySelector('.completed')) {
+            document.querySelector('.completed').remove();
+          }
+    }) 
+    saveList()
+}
+buttonRemoveCompleted()
+
+buttons('salvar-tarefas', 'Salvar Tarefas')
+
+function saveList() {
+localStorage.setItem("list_tarefas",JSON.stringify(listaTarefas))
+    
+}
