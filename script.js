a2ca5e1ad3e9a397a0d65d70061e9dceec5b7049
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-09-13 17:52:46 -0300

    Requisito 6 completo

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.onload = function () {
-  let adicionaTarefa = document.getElementById('criar-tarefa'); // Botão de criar tarefa
-  let tarefa = document.getElementById('texto-tarefa').value; // Conteúdo no campo de adicionar tarefa
-  let lista = document.getElementById('lista-tarefas'); // lista de tarefas
+let adicionaTarefa = document.getElementById('criar-tarefa'); // Botão de criar tarefa
+let tarefa = document.getElementById('texto-tarefa'); // Conteúdo no campo de adicionar tarefa
+let lista = document.getElementById('lista-tarefas'); // lista de tarefas
 
-  adicionaTarefa.addEventListener('click', () => {
-    const novoItem = document.createElement('li');
-    novoItem.innerText = tarefa;
-    lista.appendChild(novoItem);
-    tarefa = '';
-  });
-};
+adicionaTarefa.addEventListener('click', () => {
+  let novoItem = document.createElement('li');
+  novoItem.innerText = tarefa.value;
+  lista.appendChild(novoItem);
+  tarefa.value = '';
+});
 
 /* ao clicar no botão, deve ser criado uma nova linha na ol (addEventListener + createElement)
    depois, pega o texto que está na tarefa e adiciona na linha criada
