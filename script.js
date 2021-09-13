217ca25a652d43534958eb9ac42cf0f54f4fe438
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-13 15:18:05 -0300

    requisito 7 concluido

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let taskButton = document.getElementById('criar-tarefa');
 let inputTask = document.getElementById('texto-tarefa');
 
+// Função que adiciona uma nova tarefa à lista
 function addNewTask() {
   let newListItem = document.createElement('li');
+  newListItem.className = 'list-item';
   newListItem.innerHTML = inputTask.value;
   document.getElementById('lista-tarefas').appendChild(newListItem);
 
@@ -10,3 +12,14 @@ function addNewTask() {
 }
 
 taskButton.addEventListener('click', addNewTask);
+
+let listItens = document.getElementsByClassName('list-item');
+
+// Função que seleciona a tarefa
+function selectListItem(event) {
+  if (event.target.classList.contains('list-item')) {
+    event.target.classList.add('selected');
+  }
+}
+
+document.addEventListener('click', selectListItem)
