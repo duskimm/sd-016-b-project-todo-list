781200833a9051ee43b7248d507ae1a23adcdce5
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2021-09-13 16:17:10 -0300

    Requisitos 7,8 e 9.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Constantes
 const task = document.querySelector("#texto-tarefa");
 const newTask = document.querySelector("#criar-tarefa");
 const list = document.querySelector("#lista-tarefas");
+// add a new task
 function addTask() {
   const newLi = document.createElement("li");
   const text = task.value;
@@ -9,3 +11,25 @@ function addTask() {
   task.value = "";
 };
 newTask.addEventListener("click", addTask);
+// BackGroundColor change
+function selectedItem(event) {
+  const allTasks = document.querySelectorAll("li");
+  if (event.target.classList.contains("selected")) {
+    event.target.classList.remove("selected");
+  } else {
+    for (index = 0; index < allTasks.length; index += 1) {
+      allTasks[index].classList.remove("selected");
+    }
+    event.target.classList.add("selected");
+  }
+}
+list.addEventListener("click", selectedItem);
+// Completed tasks
+function completedTask(event) {
+  if (event.target.classList.contains("completed")){
+    event.target.classList.remove("completed");
+  } else {
+    event.target.classList.add("completed");
+  }
+}
+list.addEventListener("dblclick", completedTask);
