6bb15b663cd416713d1d1dd556b4742e33b0c1bf
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-09-13 15:00:55 -0300

    Requisito 7 ok

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const getInput = document.getElementById('texto-tarefa');
 const getButton = document.getElementById('criar-tarefa');
 const getOrderlist = document.getElementById('lista-tarefas');
 
+// Adicinar valores a lista 
 getButton.addEventListener('click', function () {
   const criali = document.createElement('li');
   criali.innerHTML = getInput.value;
+  criali.className = 'itemList';
   getOrderlist.appendChild(criali);
   getInput.value = '';
-})
+});
+
+getOrderlist.addEventListener('click', function(event){
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
