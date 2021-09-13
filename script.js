1bc9a337d470eb92f0d96196e5f43fb9b4dd51b6
Hiago Briano <hiago.artist@hotmail.com>
2021-09-13 16:19:12 -0300

    requisito 7 feito

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let novaTarefa = document.querySelector('input');
 let botao = document.getElementById('criar-tarefa');
 let listaDeTarefas = document.getElementById('lista-tarefas');
+// let
 
-botao.addEventListener('click', demostrar);
+botao.addEventListener('click', botaoNovaTarefa);
 
-function demostrar (){
+function botaoNovaTarefa (){
   let RecuperandoNovaTarefa = novaTarefa.value;
 
   // criando o Li da lista - Coloquei a casse tarefa - adicionei a nova tarefa
@@ -12,9 +13,21 @@ function demostrar (){
   CapturandoLI.classList.add('tarefa');
   CapturandoLI.innerText = RecuperandoNovaTarefa;
 
+  // adiciona o evento de clique quando cria o botão
+  CapturandoLI.addEventListener('click', selecionando);
+
   // adicionei o Li criado com a tarefa e a classe
   listaDeTarefas.appendChild(CapturandoLI);
 
   // apaga o campo de digitação
   novaTarefa.value = '';
+
+  // seleciona o item criando a classe selecionado
+  let tarefa = document.querySelector('.tarefa');
+  
+  function selecionando (event){
+  let evento = event.target;
+  evento.classList.add('selecionado');
+  }
 }
+
