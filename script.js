d059c0b98a2ffb96f6dc435f89d26948ab6b0f5e
Izabela <izabelahammer@gmail.com>
2021-09-13 17:58:33 -0300

    requisito passou no npm test

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* ao clicar nesse botão, um novo item deverá ser criado ao final da lista
- e o texto do input deve ser limpo
-
-No campo de input será digitado o texto de uma tarefa qualquer e,
- em seguida, clicar-se-á no botão de criar tarefa. Será verificado que,
-  após o clique, o texto digitado aparece na lista e desaparece do input.
-
-A adição de elementos na lista será feita algumas vezes, e será checado
- se todos os itens criados permanecem na lista na medida em que novos 
- são adicionados. */
 // variaveis e constantes globais utilizadas em funcoes
 let toDoList = document.querySelector('#lista-tarefas');  // ol
 let createNewItemList = document.getElementById('criar-tarefa'); //button
@@ -15,11 +5,13 @@ let createNewItemList = document.getElementById('criar-tarefa'); //button
 // funcao que insere o elemento il na ol atraves do input pelo usuario
 function insertElementIL() {
   let newItem = document.getElementById('texto-tarefa').value; //valor do input
-  const createItem = document.createElement('il'); // criando il filho 
-  createItem.innerText = newItem;
-  toDoList.appendChild(createItem);
-  const input = document.getElementById('texto-tarefa');
-  input.value = '';
+  const createItem = document.createElement('li'); // criando elemento il 
+  createItem.innerHTML = newItem;  // coloca conteudo do input no elemento il
+  toDoList.appendChild(createItem); // coloca a il dentro do ol como filho
+  const input = document.getElementById('texto-tarefa'); // pega o input
+  input.value = '';  // coloca valor vazio no input
+  createItem.style.display = 'block';   // colocar as il em lista 
 }
-insertElementIL();
-createNewItemList.addEventListener('click',insertElementIL);
+
+createNewItemList.addEventListener('click',insertElementIL); // se clicar no botao executa a funcao inserir elemento
+
