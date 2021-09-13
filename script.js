2afb5dc00b0cd4c361d3977e5809c992041adfcf
Airtonsilv4 <airton.silva19@hotmail.com>
2021-09-13 17:07:03 -0300

    Requisito 07 08

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const toDoList = document.querySelector('#lista-tarefas');
 
 function addToList() {
-  const doList = document.getElementById('lista-tarefas');
   const list = document.createElement('li');
   const input = document.getElementById('texto-tarefa');
   list.innerText = input.value;
   toDoList.appendChild(list);
-  input.value = "";
+  input.value = '';
+  click();
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/removeProperty
+function markText(event) {
+  const paintGrey = event.target;
+  for (let index = 0; index < document.getElementsByTagName('li').length; index += 1) {
+    document.getElementsByTagName('li')[index].style.removeProperty('background-color');
+  }
+  paintGrey.style.backgroundColor = 'grey';
+}
+
+function click() {
+  for (let index = 0; index < document.getElementsByTagName('li').length; index += 1){
+    document.getElementsByTagName('li')[index].addEventListener('click', markText)
+  }
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', addToList);
