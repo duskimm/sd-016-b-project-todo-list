f5d30eae7b0deeda7a8f1ebb54c58e76e47567c5
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-13 20:48:56 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//Iniciando o projeto
+const button = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+
+function addTask() {
+  button.addEventListener('click', () => {
+    const task = document.createElement('li');
+    task.innerHTML = input.value;
+    taskList.appendChild(task);
+    input.value = '';
+  });
+}
+addTask();
