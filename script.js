d90756092bb6ab85556d055a2ceb0a11f403d65e
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-13 15:21:37 -0300

    requisito 8 concluido

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ function addNewTask() {
 
 taskButton.addEventListener('click', addNewTask);
 
-let listItens = document.getElementsByClassName('list-item');
 
 // Função que seleciona a tarefa
 function selectListItem(event) {
+  let listItens = document.getElementsByClassName('list-item');
+  for (let index = 0; index < listItens.length; index += 1) {
+    listItens[index].classList.remove('selected');
+  }
+
   if (event.target.classList.contains('list-item')) {
     event.target.classList.add('selected');
   }
 }
 
-document.addEventListener('click', selectListItem)
+document.addEventListener('click', selectListItem);
