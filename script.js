170b3d9ad2c50e35e9712910b9a21c4e8ffeadc1
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-13 16:26:30 -0300

    adiciona requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // EXercicio 5
-
+// cria botao
 let criaBotao = document.createElement('button');
 criaBotao.type = 'button';
 criaBotao.id = 'criar-tarefa';
@@ -14,10 +14,20 @@ let input = document.getElementById('texto-tarefa');
 // criando lista
 botaoAddTarefa.addEventListener('click', function(){
     let criaItem = document.createElement('li');
+    criaItem.className = 'item-lista'
     lista.appendChild(criaItem);
     criaItem.innerText = input.value;
     input.value = ''
 });
+
+// Exercico 7: mudar background quando clica
+function listaBackground(event){
+    if(event.target.classList.contains('item-lista')) {
+        let li = event.target;
+        li.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+}
+document.addEventListener('click', listaBackground);
 
 
 
