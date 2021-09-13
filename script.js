49dc228094c2d818b7380b4f22a0a63611ed1d85
Matheus <matheusmoura2110@gmail.com>
2021-09-13 17:48:36 -0300

    arruma o lint

--- script.js
+++ script.js
@@ -2,56 +2,55 @@ function addtask() {
   const createTask = document.getElementById('texto-tarefa').value;
   let list = document.getElementById('lista-tarefas').innerHTML;
 
-  list += '<li>' + createTask + '</li>';
+  list += `<li>${createTask}</li>`;
   document.getElementById('lista-tarefas').innerHTML = list;
   document.getElementById('texto-tarefa').value = null;
 }
+addtask();
 
-//indicação para tirar ideia do video: https://www.youtube.com/watch?v=pRwxgtqImZQ
-//altera o fundo do texto clicado para cinza 
+// indicação para tirar ideia do video: https://www.youtube.com/watch?v=pRwxgtqImZQ
+// altera o fundo do texto clicado para cinza
 const toGray = document.getElementById('lista-tarefas');
-function changeColor(event) {
-  const selectedItem = document.querySelectorAll('li')
+function changeColor(e) {
+  const selectedItem = document.querySelectorAll('li');
   for (let index = 0; index < selectedItem.length; index += 1) {
-    selectedItem[index].style.backgroundColor = ""
+    selectedItem[index].style.backgroundColor = '';
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 toGray.addEventListener('click', changeColor);
 
-
-//adiciona uma linha no meio do texto qunado clicado duas vezes
-function lineThrough(event) {
-  if (event.target.className === "completed") {
-    event.target.className = ""
+// adiciona uma linha no meio do texto qunado clicado duas vezes
+function lineThrough(e) {
+  if (e.target.className === 'completed') {
+    e.target.className = '';
   } else {
-    event.target.className = "completed"
+    e.target.className = 'completed';
   }
 }
-toGray.addEventListener("dblclick", lineThrough);
+toGray.addEventListener('dblclick', lineThrough);
 
-
-//limpa a lista de itens
+// limpa a lista de itens
 const clearitens = document.getElementById('apaga-tudo');
 function removeAll() {
-  let deleteAll = document.querySelectorAll('li')
+  const deleteAll = document.querySelectorAll('li');
   if (deleteAll.length > 0) {
-    for (index = 0; index < deleteAll.length; index += 1) {
+    for (let index = 0; index < deleteAll.length; index += 1) {
       deleteAll[index].remove();
     }
   }
 }
 clearitens.addEventListener('click', removeAll);
 
-//remove todos os itens com a classe .completed
+// remove todos os itens com a classe .completed
 const removeFinishedItens = document.getElementById('remover-finalizados');
-removeFinishedItens.addEventListener('click', removeRiscado);
-function removeRiscado() {
-  let completedTasks = document.querySelectorAll('.completed')
-  console.log(completedTasks)
+function removeLinedItem() {
+  const completedTasks = document.querySelectorAll('.completed');
+  console.log(completedTasks);
   if (completedTasks.length > 0) {
-    for (index = 0; index < completedTasks.length; index += 1) {
-      completedTasks[index].remove()
+    for (let index = 0; index < completedTasks.length; index += 1) {
+      completedTasks[index].remove();
     }
   }
 }
+removeFinishedItens.addEventListener('click', removeLinedItem);
