8e78f5e65c357355ae15e619117a72d8accabad6
Everton-Curti <evercurti@gmail.com>
2021-09-13 15:16:17 -0300

    exercicio 2 resolvido

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let capturarCabecalho = document.querySelector('#cabecalho');
 let criarCabecalho = document.createElement('h1');
 criarCabecalho.innerHTML = 'Minha Lista de Tarefas';
 capturarCabecalho.appendChild(criarCabecalho);
+
+let capturarParagrafo = document.querySelector('p');
+capturarParagrafo.id = 'funcionamento';
+capturarParagrafo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
