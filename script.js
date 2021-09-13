74416652f84b6591ee9f1e9c2b2f6aff98d63e2f
victor_calidor <victor.calidor@gmail.com>
2021-09-13 17:00:36 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,11 +3,16 @@
 const button = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 
+function mudarCor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function criarTarefa() {
   const inputTexto = document.getElementById('texto-tarefa').value;
   const novaTarefa = document.createElement('li');
   novaTarefa.innerText = inputTexto;
   document.getElementById('texto-tarefa').value = '';
+  novaTarefa.addEventListener('click', mudarCor);
   lista.appendChild(novaTarefa);
 }
 
