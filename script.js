ede8042e737f4050faba82610c57902b2a5cedb4
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-13 18:54:25 -0300

    C6 Requisito 7 e 8 - Fundo cinza em somente um elemento

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ function criarTarefa() {
   document.querySelector('#texto-tarefa').value = '';
 }
 
+function bgGray(event) {
+  const eventTarget = event.target;
+  const bgGraySelector = document.getElementsByClassName('bgCinza');
+  if (bgGraySelector.length > 0) {
+    bgGraySelector[0].style.backgroundColor = '#7986ac';
+    bgGraySelector[0].classList.remove('bgCinza');
+    eventTarget.className = 'bgCinza';
+    bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
+  } else {
+    eventTarget.className = 'bgCinza';
+    bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
+  }
+}
+
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 btnCriarTarefa.addEventListener('click', criarTarefa);
+const olLi = document.querySelector('#lista-tarefas');
+olLi.addEventListener('click', bgGray);
