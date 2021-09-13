162c54f3ac7f0fc72e03be9174f41e9410be61a0
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-09-13 16:54:38 -0300

    requisito 7, requisito 8

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
+const ol = document.getElementById('lista-tarefas');
+const listTask = document.getElementsByTagName('li');
 button.addEventListener('click', () => {
-  const ol = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
   li.innerText = input.value;
   ol.appendChild(li);
   input.value = '';
+});
+
+ol.addEventListener('click', (event) => {
+  const evento = event.target;
+  for (let index = 0; index < listTask.length; index += 1) {
+    listTask[index].style.backgroundColor = 'white';
+  }
+  evento.style.backgroundColor = 'rgb(128, 128, 128)';
 });
