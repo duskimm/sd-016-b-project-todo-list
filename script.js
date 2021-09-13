ef4fc7d28122a8842bc6fcac522f2f6f381a1473
AirtonL <junynnhoh@gmail.com>
2021-09-13 16:30:23 -0300

    teste erro node

--- script.js
+++ script.js
@@ -6,8 +6,49 @@ function addItemList() {
     const criaLi = document.createElement('li');
     const localizaInput = document.getElementById('texto-tarefa');
     criaLi.innerText = localizaInput.value;
+    criaLi.className = 'list';
     localizaOl.appendChild(criaLi);
     localizaInput.value = '';
   });
 }
 addItemList();
+
+function removeColor() {
+  const removeBackGroundColor = document.querySelectorAll('.list');
+
+  for (let index = 0; index < removeBackGroundColor.length; index += 1) {
+    if (removeBackGroundColor[index].classList.contains('selected')) {
+      removeBackGroundColor[index].classList.remove('selected');
+      removeBackGroundColor[index].style.backgroundColor = '';
+    }
+  }
+}
+
+function altCor() {
+  const addColor = document.querySelectorAll('ol');
+
+  for (let index = 0; index < addColor.length; index += 1) {
+    addColor[index].addEventListener('click', (event) => {
+      removeColor();
+      const evento = event.target;
+      evento.classList.add('selected');
+      evento.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+altCor();
+
+function doubleClick() {
+  const localizaOl = document.querySelectorAll('ol');
+
+  for (let index = 0; index < localizaOl.length; index += 1) {
+    localizaOl[index].addEventListener('dblclick', (event) => {
+      if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+      } else {
+        event.target.classList.add('completed');
+      }
+    });
+  }
+}
+doubleClick();
