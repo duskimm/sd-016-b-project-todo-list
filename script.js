d4b041a32baf27c6190de35917fa0132ea54fbb9
Gabriel A <gabrielaug4@gmail.com>
2021-09-13 16:38:17 -0300

    req.09

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+function alterarCorDeFundoDaLi(event) {
+  const tarefa = event.target;
+  const li = document.querySelectorAll('li');
+  for (let index = 0; index < li.length; index += 1) {
+    li[index].style.backgroundColor = 'white';
+  }
+  tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function riscarTarefa(event) {
+  const tarefa = event.target;
+  if (tarefa.classList.contains('completed')) {
+    tarefa.classList.remove('completed');
+  } else {
+    tarefa.classList.add('completed');
+  }
+}
+
 function criarNovaTarefa() {
   const criarTarefa = document.getElementById('criar-tarefa');
   criarTarefa.addEventListener('click', function () {
     const textoTarefa = document.getElementById('texto-tarefa');
     const itemDaLista = document.createElement('li');
     itemDaLista.addEventListener('click', alterarCorDeFundoDaLi);
+    itemDaLista.addEventListener('dblclick', riscarTarefa);
     const listaTarefas = document.getElementById('lista-tarefas');
     itemDaLista.innerHTML = textoTarefa.value; 
     textoTarefa.value = '';
@@ -11,13 +30,4 @@ function criarNovaTarefa() {
   });
 }
 criarNovaTarefa();
-
-function alterarCorDeFundoDaLi (event) {
-  const tarefa = event.target;
-  const li = document.querySelectorAll('li');
-  for (index = 0; index < li.length; index += 1) {
-    li[index].style.backgroundColor = 'white';
-  }
-  tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
-}
 
