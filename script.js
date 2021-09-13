90de1d4d1a98c58a2cd2270a65a96e14621bc2f8
MychaelJales <mychael_duarte@hotmail.com>
2021-09-13 16:53:45 -0300

    Finalizando os itens do 6 e 7

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const addTarefa = document.getElementById('criar-tarefa');
 
-function addTarefaFunction() {
-  const todoList = document.getElementById('lista-tarefas');
+const todoList = document.getElementById('lista-tarefas');
 
+function addTarefaFunction() {
   const createLi = document.createElement('li');
 
   const textTodo = document.getElementById('texto-tarefa');
@@ -11,7 +11,24 @@ function addTarefaFunction() {
 
   createLi.innerText = textTodo.value;
 
+  createLi.classList.add('item-list');
+
   textTodo.value = '';
 }
 
+function checkClass() {
+  const arrayClassList = document.getElementsByClassName('item-selected');
+  for (let index = 0; index < arrayClassList.length; index += 1) {
+    arrayClassList[index].className = 'item-list';
+  }
+}
+
+function selectedItem(event) {
+  checkClass();
+  const clickItem = event.target;
+  clickItem.classList.add('item-selected');
+}
+
 addTarefa.addEventListener('click', addTarefaFunction);
+
+todoList.addEventListener('click', selectedItem);
