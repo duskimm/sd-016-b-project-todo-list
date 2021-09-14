b2f5e1d0150e874a7ff49c5c891c29ac3aea54a7
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-14 15:50:49 -0300

    C7 Requisito 9 - dbclick - completed

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
+const listaTarefa = document.querySelector('#lista-tarefas');
+
 function criarTarefa() {
   const inputText = document.querySelector('#texto-tarefa').value;
-  const listaTarefa = document.querySelector('#lista-tarefas');
   const newLi = document.createElement('li');
   newLi.innerText = inputText;
   listaTarefa.appendChild(newLi);
   document.querySelector('#texto-tarefa').value = '';
 }
 
+function completedClass(evento) {
+  const eventoTarget = evento.target;
+  if (eventoTarget.classList.contains('completed')) {
+    eventoTarget.classList.remove('completed');
+  } else {
+    eventoTarget.classList.add('completed');
+  }
+}
+
 function bgGray(event) {
   const eventTarget = event.target;
   const bgGraySelector = document.getElementsByClassName('bgCinza');
-  if (bgGraySelector.length > 0) {
-    bgGraySelector[0].style.backgroundColor = '#7986ac';
-    bgGraySelector[0].classList.remove('bgCinza');
-    eventTarget.className = 'bgCinza';
-    bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
-  } else {
-    eventTarget.className = 'bgCinza';
-    bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
+  if (eventTarget !== listaTarefa) {
+    if (bgGraySelector.length > 0) {
+      bgGraySelector[0].style.backgroundColor = '#c2bfaf';
+      bgGraySelector[0].classList.remove('bgCinza');
+      eventTarget.classList.add('bgCinza');
+      bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
+    } else {
+      eventTarget.classList.add('bgCinza');
+      bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
+    }
   }
 }
 
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 btnCriarTarefa.addEventListener('click', criarTarefa);
-const olLi = document.querySelector('#lista-tarefas');
-olLi.addEventListener('click', bgGray);
+
+listaTarefa.addEventListener('click', bgGray);
+const olLi2 = document.querySelector('.div_lista-tarefas');
+olLi2.addEventListener('dblclick', completedClass);
+// se estiver selecionado, deselecione
