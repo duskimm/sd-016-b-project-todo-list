8c32b92e30df3b075f753cffd74db3cf680048e1
Douglas Toledo <Douglas Toledo - Duskimm>
2021-09-14 11:42:18 -0300

    requisito 07

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // CRIAR TODO HTML NO JS
 // TALVEZ MANIPULAR O CSS PELO JS TBM (O menino está ousado!!! kkkkkkk)
 
+
 // 1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
 
 const header = document.createElement('header');
@@ -12,6 +13,7 @@ headH1.id = 'title';
 headH1.append('Minha Lista de Tarefas');
 header.append(headH1);
 
+
 // 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
 
 const headPara = document.createElement('p')
@@ -19,12 +21,14 @@ headPara.id = 'funcionamento';
 headPara.append('Clique duas vezes em um item para marcá-lo como completo');
 header.append(headPara);
 
+
 // // 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
 
 const input01 = document.createElement('input');
 input01.type = 'text';
 input01.id = 'texto-tarefa';
 document.body.append(input01);
+
 
 //  4 - Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
 
@@ -34,21 +38,52 @@ document.body.append(ordList);
 
 
 // 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+// 6 - Ordene os itens da lista de tarefas por ordem de criação
 
 const button01 = document.createElement('button');
 button01.setAttribute('onclick', 'addLi()');
 button01.id = 'criar-tarefa';
-button01.innerText = 'adicionar';
+button01.innerText = 'Cria Tarefa';
 document.body.append(button01);
 
 function addLi() {
-  let txtVal = document.getElementById('texto-tarefa').value;
-  let listNode = document.getElementById('lista-tarefas');
-  let liNode = document.createElement('li');
-  let txtNode = document.createTextNode(txtVal);
 
-  liNode.appendChild(txtNode);
-  listNode.appendChild(liNode);
+  let txtVal = document.getElementById('texto-tarefa').value;
+
+  if( txtVal.length > 0) {
+
+    let listNode = document.getElementById('lista-tarefas');
+    let liNode = document.createElement('li');
+    let itemList = document.createElement('a');
+    let txtNode = document.createTextNode(txtVal);
   
-  this.onclick = document.getElementById('texto-tarefa').value = ''; // APAGANDO
+    itemList.appendChild(txtNode);
+    listNode.appendChild(liNode);
+    liNode.append(itemList);
+    
+    this.onclick = document.getElementById('texto-tarefa').value = ''; // APAGANDO
+  }
 }
+
+
+// 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
+
+// course 5.3 part 2
+
+const firstLi = document.getElementById('lista-tarefas');
+
+function selectingItem(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+firstLi.addEventListener('click', selectingItem);
+
+
+
+
+// function unselectingItem(event) {
+
+//   event.target.style = 'none';
+// }
+// firstLi.addEventListener('click', unselectingItem);
+
