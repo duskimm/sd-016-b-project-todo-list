4547cbe5b9c96239425d1b74f63f5539c88851dd
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-13 21:41:47 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+const task = document.getElementsByClassName('tarefa');
+
+function deselectAllItems() {
+  for (let index = 0; index < task.length; index += 1) {
+    if (task[index].classList.contains('selected') === true) {
+      task[index].classList.toggle('selected');
+    }
+  }
+}
+
+function selectItem() {
+  for (let index = 0; index < task.length; index += 1) {
+    task[index].addEventListener('click', () => {
+      deselectAllItems();
+      task[index].classList.toggle('selected');
+    });
+  }
+}
 
 function addTask() {
   button.addEventListener('click', () => {
-    const task = document.createElement('li');
-    task.innerHTML = input.value;
-    taskList.appendChild(task);
+    const newTask = document.createElement('li');
+    newTask.innerHTML = input.value;
+    newTask.classList.add('tarefa');
+    taskList.appendChild(newTask);
     input.value = '';
+    selectItem();
   });
 }
-addTask();
+
+window.onload = () => {
+  addTask();
+  deselectAllItems();
+};
