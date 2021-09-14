103f3a1d8d053bde9b9a1b24a628999c72ea1055
André Melo <adr.smelo@gmail.com>
2021-09-14 17:10:38 -0300

    Correcao lint

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ const addListOl = document.getElementById('lista-tarefas');
 const addInput = document.getElementById('texto-tarefa');
 const addBackground = document.getElementsByTagName('li');
 const deleteList = document.getElementById('apaga-tudo');
+const removerFinalizados = document.getElementById('remover-finalizados');
 
-addButton.addEventListener('click', function () {
+addButton.addEventListener('click', () => {
   const createLi = document.createElement('li');
   createLi.className = 'itens';
   createLi.innerText = addInput.value;
   addListOl.appendChild(createLi);
-  addInput.value = "";
+  addInput.value = '';
 });
 
-addListOl.addEventListener('click', function (event) {
+addListOl.addEventListener('click', (event) => {
   for (let index = 0; index < addBackground.length; index += 1) {
     addBackground[index].style.backgroundColor = 'white';
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
 
-addListOl.addEventListener('dblclick', function (event) {
+addListOl.addEventListener('dblclick', (event) => {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
@@ -27,16 +28,14 @@ addListOl.addEventListener('dblclick', function (event) {
   }
 });
 
-deleteList.addEventListener('click', function () {
-  const itens = document.querySelectorAll('.itens'); //addListOl.innerHTML = '';
+deleteList.addEventListener('click', () => {
+  const itens = document.querySelectorAll('.itens');// addListOl.innerHTML = '';
   for (let index = 0; index < itens.length; index += 1) {
-    addListOl.removeChild(itens[index]); //itens[index].remove() + for.
+    addListOl.removeChild(itens[index]);// itens[index].remove() + for.
   }
 });
 
-const removerFinalizados = document.getElementById('remover-finalizados')
-
-removerFinalizados.addEventListener('click', function () {
+removerFinalizados.addEventListener('click', () => {
   const completed = document.querySelectorAll('.completed');
   for (let index = 0; index < completed.length; index += 1) {
     addListOl.removeChild(completed[index]);
