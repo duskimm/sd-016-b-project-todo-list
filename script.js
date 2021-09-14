f57e5ce799bdae3aed157862bd2cf7aae4822914
Davi <davivarejao@gmail.com>
2021-09-14 18:37:12 -0300

    cumpre requisitos 6, 7 e 8

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ window.onload = function () {
     createTaskButton.addEventListener("click", function () {
         let text = document.createElement("li");
         text.innerText = inputText.value;
+        text.className = "tarefa";
         taskList.appendChild(text);
         inputText.value = "";
+    })
+
+    // Requisito 6
+
+    let listItens = document.getElementsByClassName("tarefa");
+
+    document.addEventListener("click", function(event){
+        if (event.target.classList.contains("tarefa")) {
+            for (let i = 0; i < listItens.length; i ++) {
+                listItens[i].classList.remove("selected");
+            }
+            event.target.classList.add("selected");
+        }
     })
 }
