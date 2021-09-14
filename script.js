948137b028c9dfdc85d44589e27164b6db2e8868
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-14 01:37:59 -0300

    Adicionado requisito 11

--- script.js
+++ script.js
@@ -97,3 +97,21 @@ function newClearBtn() {
   });
 }
 newClearBtn();
+
+function newCompletedBtn() {
+  const completedBtn = document.createElement('button');
+  completedBtn.id = 'remover-finalizados';
+  completedBtn.innerText = 'Remover Finalizadas';
+
+  main.appendChild(completedBtn);
+
+  completedBtn.addEventListener('click', () => {
+    const taskList = document.getElementById('lista-tarefas');
+    const completedList = document.querySelectorAll('.completed');
+
+    for (let i = completedList.length - 1; i >= 0; i -= 1) {
+      taskList.removeChild(completedList[i]);
+    }
+  });
+}
+newCompletedBtn();
