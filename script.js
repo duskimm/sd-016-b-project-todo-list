6b19c4456f4b8319d2c01def30338d867806348a
Celso Fiorani <celsofiorani@gmail.com>
2021-09-14 15:40:28 -0300

    parte 5 e 6

--- script.js
+++ script.js
@@ -13,10 +13,14 @@ const textoTarefa = document.getElementById('texto-tarefa');
 // 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado 
 // ao final da lista e o texto do input deve ser limpo
 
-function addTarefa(){
-    // CRIA A LI DENTRO DO #LISTA-TAREFAS
-     //FAZ O LI FICAR COMO FILHO DA OL
-     // o texto da LI recebe o valor do INPUT
+function addTarefa() {
+  let criaLi = document.createElement('li'); // CRIA A LI DENTRO DO #LISTA-TAREFAS
+  listaDeTarefas.appendChild(criaLi); //FAZ O LI FICAR COMO FILHO DA OL
+  criaLi.innerText = textoTarefa.value // o texto da LI recebe o valor do INPUT
+  textoTarefa.value = ""; // reseta o valor do input
 }
- // O EVENTO PRECISA SER NO BOTAO
- 
+btnTarefa.addEventListener('click', addTarefa) // O EVENTO PRECISA SER NO BOTAO
+
+//7 
+// 
+rgb(128,128,128)
