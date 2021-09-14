5a968fe2308c2a545d9e96329dd81d5b6c364449
Istvan <olivaistvan@gmail.com>
2021-09-14 17:32:18 -0300

    Refatora 09 e resolve 11

--- script.js
+++ script.js
@@ -28,18 +28,28 @@ function pintarCinza(event) {
 list.addEventListener('click', pintarCinza);
 
 //  Requisito 09  //
-function clicks(event){
-    let classeCompleted = document.querySelector('.completed')
-    let clicado = event.target;
-    if(clicado.className = classeCompleted){
-        clicado.classList.remove('completed')
-    } else {
-    clicado.classList.add('completed')
-    }
-  };
+function clicks (event){
+  if (event.target.classList.contains('completed')){
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
 list.addEventListener('dblclick', clicks);
 
 // Requisito 10 //
 clear.addEventListener('click', function(){
     list.innerHTML = '';
 });
+
+// Requisito 11 //
+const removeFi = document.getElementById('remover-finalizados');
+
+function removeFinalizados (){
+  const completed = document.querySelectorAll('.completed');
+    for (let ind = 0; ind < completed.length ; ind += 1 ){
+         completed[ind].remove();             
+    }
+}
+removeFi.addEventListener('click', removeFinalizados);
+
