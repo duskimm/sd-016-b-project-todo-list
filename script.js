a4b9d459636559a0a74cf4fe85d18a565e69938f
Renata <renatamagno@live.com>
2021-09-14 18:54:26 -0300

    Requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const elementList = document.getElementById('lista-tarefas');
 const elementInput = document.querySelector('input');
-const elementButton = document.querySelector('button');
+const elementButtonAdd = document.querySelector('button');
 const elementItemList = document.getElementsByClassName('itemList');
-
+const getListItem = document.getElementsByTagName('li')
+const elementClear = document.getElementById('apaga-tudo')
 const tasks = [];
 
 // Função para criar elementos de lista
@@ -21,22 +22,48 @@ createToDoList();
 
 // Função para adicionar textos dos inputs à lista.
 function addTask() {
-  const textTask = elementInput.value;
-  tasks.push(textTask);
-  elementInput.value = '';
-  createToDoList();
+  if (elementInput.value != ''){
+    const textTask = elementInput.value;
+    tasks.push(textTask);
+    elementInput.value = '';
+    createToDoList();
+  }
 }
 
-elementButton.addEventListener('click', addTask);
+elementButtonAdd.addEventListener('click', addTask);
 
 // Função para aterar backgroundColor quando clicar no item da lista
 
 function changeBackgroudColor (event) {
-    if (event.target.classList.contains('itemList')){
-      for (let i = 0; i < elementItemList.length; i += 1) {
-        elementItemList[i].classList.remove('selected');
-      }
-      event.target.classList.add('selected');
+  if (event.target.classList.contains('itemList')){
+    for (let i = 0; i < elementItemList.length; i += 1) {
+      elementItemList[i].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
   }
 }
 document.addEventListener('click', changeBackgroudColor);
+
+
+// Função para riscar item completado
+
+function doubleClickTask(event) {
+  if (event.target.classList.contains('completed')){
+    event.target.classList.remove('completed')
+  } else {
+    event.target.classList.add('completed')
+  }
+}
+
+document.addEventListener('dblclick', doubleClickTask);
+
+// função limpar tarefas
+
+const clearTasks = (event) => {
+  const todoList = document.getElementById ('lista-tarefas');
+  while (todoList.firstChild){
+    todoList.removeChild (todoList.lastChild);
+  }
+}
+
+elementClear.addEventListener('click', clearTasks )
