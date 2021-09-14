d0aa8ec87a27943dad9361cd7d7a0b084f463b97
Marcio Junior <marcio.mj70@gmail.com>
2021-09-14 14:35:17 -0300

    add background color to selected task

--- script.js
+++ script.js
@@ -2,9 +2,34 @@ const taskInput = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const addTaskBtn = document.getElementById('criar-tarefa');
 
+function addBackgroundToTask(tasks) {
+  let taskState = {
+    selected: false,
+  };
+  tasks.forEach((task) => {
+    task.addEventListener('click', (event) => {
+      const selectedTask = event.target;
+
+      if (!taskState.selected) {
+        selectedTask.style.backgroundColor = 'gray';
+      } else {
+        selectedTask.style.backgroundColor = 'white';
+      }
+
+      taskState = {
+        selected: !taskState.selected,
+      };
+    });
+  });
+}
+
 addTaskBtn.addEventListener('click', () => {
-  const task = document.createElement('li');
-  task.innerText = taskInput.value;
-  taskList.appendChild(task);
+  const taskElement = document.createElement('li');
+  taskElement.innerText = taskInput.value;
+  taskElement.classList.add('task-item');
+  taskList.appendChild(taskElement);
   taskInput.value = '';
+
+  const tasks = document.querySelectorAll('.task-item');
+  addBackgroundToTask(tasks);
 });
