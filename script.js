c145603167b5d8187137252f23fd1907f10acf64
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-13 23:50:40 -0300

    5 passo +-

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var input = document.querySelector("#nova-tarefa");
+function adicionarTarefa () {
+  let botaoAdicionarNovaTarefa = document.getElementById("criar-tarefa");
+  let listaTarefas = document.getElementById("lista-tarefas");
+  let novaTarefa = document.createElement("li");
+  let tarefa = document.getElementById ("texto-tarefa").value;
+    botaoAdicionarNovaTarefa.addEventListener("click", function(){
+      listaTarefas.appendChild(novaTarefa);
+      novaTarefa.innerText = tarefa;
+      document.getElementById ("texto-tarefa").value = null;
+    })
+}
 
-var texto = input.value;
+function colorirTarefa () {
+  listaTarefas.addEventListener("click", function(event){
+    let colori = event.target;
+    if (target.tagName === "li"){
+      target.style.backgroundcolor = "rgb(128, 128, 128)" 
+    }
+  })
+}
 
-console.log(texto);
+function limparLista () {
+
+}
+
+
+adicionarTarefa();
+colorirTarefa
+limparLista ();
