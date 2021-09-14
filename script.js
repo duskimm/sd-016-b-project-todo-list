3a80727b06ef186b0ff80b149e71ab3de5e510f9
Morallys <morallys.juan@gmail.com>
2021-09-14 01:08:48 -0300

    Requisitos 7 e 8 check

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const createTaskFunction = document.querySelector('#criar-tarefa');
 const listTask = document.querySelector('#lista-tarefas');
-const task = document.querySelector('#texto-tarefa');
+const taskText = document.querySelector('#texto-tarefa');
+let clickSelected = '';
+
+// Limpa o input após ser preenchido
 
 function inputClean() {
-  task.value = '';
+  taskText.value = '';
 }
+
+// Cria o evento do botão de pegar a tarefa do input
 
 createTaskFunction.addEventListener('click', () => {
   console.log('click botão nova tarefa');
-  const createTask = document.createElement('li');  
-  const textTask = task.value;
-  createTask.innerText = textTask;
+  const createTask = document.createElement('li');
+  const task = taskText.value;
+  createTask.innerText = task;
 
   listTask.appendChild(createTask);
+
   inputClean();
 });
 
-// btnAndInput.appendChild(generateBoard).innerText = 'VQV';
+// Após o click analisa se elemento contém ou não o id 'selected' e o exclue se houver. Cria o evento na variável e coloca o id selected no item da lista selecionado
+
+function clickTask() {
+  listTask.addEventListener('click', (event) => {
+    const elementList = document.querySelectorAll('#lista-tarefas li');
+    for (let index = 0; index < elementList.length; index += 1) {
+      if (elementList[index].id === 'selected' || elementList[index].id === '') {
+        elementList[index].id = '';
+      }
+    }
+    clickSelected = event.target;
+    clickSelected.id = 'selected';
+  });
+}
+
+clickTask();
