437d907ae054eb0ec4981b123c870b939400dc66
Wellington <tinhoefaela@gmail.com>
2021-09-14 15:32:31 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 
-button.addEventListener('click', () => {
-  const li = document.createElement('li');
-  li.innerText = input.value;
-  ol.appendChild(li);
-  input.value = '';
-});
+
+window.onload = () => {
+  createElementLi();
+};
+
+function createElementLi() {
+  button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    li.className = 'tarefa';
+    ol.appendChild(li);
+    input.value = '';
+})
+}
+
+function selectItem() {
+  ol.addEventListener('click', (event) => {
+    const tarefa = document.querySelectorAll('.tarefa');
+    for (let i = 0; i < tarefa.length; i += 1) {
+      if (tarefa[i].id === 'selected' || tarefa[i].id === ''){
+        event.target.setAttribute('id', 'selected');
+      }}
+    }
+  )
+}
+
+selectItem()
