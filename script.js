888a4eb7adb5e014f40eb064addfed9e0f0ca95b
Eglany Junior <contas.eglanyjr@gmail.com>
2021-09-13 20:03:18 -0400

    refatorado funcionalidades de select e completed

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const todoText = document.getElementById('texto-tarefa');
 const createButton = document.getElementById('criar-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
+const taskLi = document.getElementsByTagName('li');
 
 function createTodo() {
   const createLi = document.createElement('li');
   const text = todoText.value;
   todoList.appendChild(createLi).innerText = text;
+  todoText.value = '';
 }
 
 createButton.addEventListener('click', createTodo);
@@ -14,25 +16,35 @@ createButton.addEventListener('click', createTodo);
 //selecionar li
 let liTarget = '';
 
-function unselecteLi() {
-  if (liTarget.className === 'selected' || liTarget.className === 'selected completed') {
-    liTarget.classList.remove('selected');
-  }
-}
 function selectedLi(event) {
-  unselecteLi();
+  for(let index = 0; index < taskLi.length; index += 1 ){
+    taskLi[index].style.backgroundColor = 'white';
+  }
   liTarget = event.target;
-  liTarget.className = 'selected';
+  liTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log(taskLi);
 }
 
 todoList.addEventListener('click', selectedLi);
 
-//tarela completa ---- resolver, mais de uma selecao
+//tarefa completa
 function completTask(event) {
-  if (liTarget.className === 'selected') {
-    unselecteLi();
-    liTarget.classList = 'selected completed';
-    console.log(event.target);
+  if (liTarget.className === 'completed') {
+    liTarget.classList.remove('completed');
+  } else {
+    liTarget.classList.add('completed');
+    console.log(taskLi);
   }
 }
+
 todoList.addEventListener('dblclick', completTask);
+
+//limpar todas as tasks
+const clearButton = document.getElementById('apaga-tudo');
+
+clearButton.addEventListener('click', function() {
+  for (let index = 0; index < todoList.children.length; index = 0) {
+    todoList.removeChild(taskLi[index]);
+  }
+  console.log('ta rodando');
+});
