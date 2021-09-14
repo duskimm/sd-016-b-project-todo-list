6c0ccb0998acd180b7de5c6f34f2b740cb033b03
Renata <renatamagno@live.com>
2021-09-14 11:48:52 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const elementList = document.getElementById('lista-tarefas');
 const elementInput = document.querySelector('input');
 const elementButton = document.querySelector('button');
+const elementItemList = document.getElementsByClassName('itemList');
 
 const tasks = [];
 
-function createToDoList () {
+// Função para criar elementos de lista
+function createToDoList() {
   elementList.innerHTML = '';
-  
-  for (item of tasks) {
+  for (let item of tasks) {
     const elementTask = document.createElement('li');
+    elementTask.classList.add('itemList')
     const textTask = document.createTextNode(item);
 
     elementTask.appendChild(textTask);
     elementList.appendChild(elementTask);
+  }
+}
+createToDoList();
 
-  } 
-};
-createToDoList()
-
-function addTask () {
+// Função para adicionar textos dos inputs à lista.
+function addTask() {
   const textTask = elementInput.value;
   tasks.push(textTask);
-  elementInput. value = ''
-  createToDoList()
-};
+  elementInput.value = '';
+  createToDoList();
+}
 
 elementButton.addEventListener('click', addTask);
+
+// Função para aterar backgroundColor quando clicar no item da lista
+
+function changeBackgroudColor (event) {
+    if (event.target.classList.contains('itemList')){
+      for (let i = 0; i < elementItemList.length; i += 1) {
+        elementItemList[i].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+  }
+}
+document.addEventListener('click', changeBackgroudColor);
