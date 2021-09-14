f26b0d6a36900a2358b910e4423af5e2b52199a0
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-14 13:55:56 -0300

    adiciona requisito 9

--- script.js
+++ script.js
@@ -21,14 +21,48 @@ botaoAddTarefa.addEventListener('click', function(){
 });
 
 // Exercico 7: mudar background quando clica
-function listaBackground(event){
-    if(event.target.classList.contains('item-lista')) {
-        let li = event.target;
-        li.style.backgroundColor = 'rgb(128, 128, 128)';
+// function listaBackground(event){
+//     if(event.target.classList.contains('item-lista')) {
+//         let li = event.target;
+//         li.classList.add('cinza');
+//         li.style.backgroundColor = 'rgb(128, 128, 128)';
+//     } 
+// }
+// document.addEventListener('click', listaBackground);
+
+
+// Exercicio 8: seleciona só um
+    
+function selecionaItem(event) {
+    let classCinza = document.querySelector('cinza');
+    
+    // let fundo = window.getComputedStyle(classCinza, null).getPropertyValue('background-color');
+    
+    if(classCinza === null) {
+        event.target.classList.add('cinza');
+        // event.target.style.backgroundColor = 'red';
+        
+    } else {
+        classCinza.classList.remove('cinza');
+        
+        event.target.classList.add('cinza');
+        // event.target.style.backgroundColor = 'red';
+    }
+
+}
+lista.addEventListener('click', selecionaItem);
+
+
+// Exercicio 9: risca tarefa com dois clicks
+
+let riscado = document.getElementsByClassName('completed');
+// let estilo = window.getComputedStyle(riscado, null).getPropertyValue('text-decoration');
+
+function clicks(event) {
+    if(event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+    } else {
+        event.target.classList.add('completed');
     }
 }
-document.addEventListener('click', listaBackground);
-
-
-
-
+lista.addEventListener('dblclick', clicks);
