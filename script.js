d8a7a04e7dabb2b87b0a9e5e5a59ccd18e466acd
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-14 01:06:25 -0300

    13

--- script.js
+++ script.js
@@ -6,87 +6,87 @@ divBody.appendChild(creatHeader)
 creatHeader.appendChild(creatTitle)
 
 function paragraph(params) {
-    const creatParagraph = document.createElement('p')
-    const creatSection = document.createElement('section')
-    divBody.appendChild(creatSection)
-    creatSection.appendChild(creatParagraph)
-    creatParagraph.innerHTML = params
-    creatParagraph.id = 'funcionamento'
+  const creatParagraph = document.createElement('p')
+  const creatSection = document.createElement('section')
+  divBody.appendChild(creatSection)
+  creatSection.appendChild(creatParagraph)
+  creatParagraph.innerHTML = params
+  creatParagraph.id = 'funcionamento'
 }
 paragraph('Clique duas vezes em um item para marcá-lo como completo')
 const creatSectionButtons = document.createElement('section')
 divBody.appendChild(creatSectionButtons)
 
 function inputList() {
-    const creatDivInput = document.createElement('div')
-    const creatInput = document.createElement('input')
-    creatInput.id = 'texto-tarefa'
-    divBody.appendChild(creatDivInput)
-    creatDivInput.appendChild(creatInput)
+  const creatDivInput = document.createElement('div')
+  const creatInput = document.createElement('input')
+  creatInput.id = 'texto-tarefa'
+  divBody.appendChild(creatDivInput)
+  creatDivInput.appendChild(creatInput)
 }
 inputList()
 
 function List() {
-    const creatTodoList = document.createElement('ol')
-    const creatMain = document.createElement('main')
-    creatTodoList.id = 'lista-tarefas'
-    divBody.appendChild(creatMain)
-    creatMain.appendChild(creatTodoList)
+  const creatTodoList = document.createElement('ol')
+  const creatMain = document.createElement('main')
+  creatTodoList.id = 'lista-tarefas'
+  divBody.appendChild(creatMain)
+  creatMain.appendChild(creatTodoList)
 
 }
 List()
 
 function buttons(id, nome) {
-    const creatButtons = document.createElement('button')
-    creatSectionButtons.id = "buttons"
-    creatButtons.id = id
-    creatButtons.innerHTML = nome
-    creatSectionButtons.appendChild(creatButtons)
+  const creatButtons = document.createElement('button')
+  creatSectionButtons.id = "buttons"
+  creatButtons.id = id
+  creatButtons.innerHTML = nome
+  creatSectionButtons.appendChild(creatButtons)
 
 }
 buttons('criar-tarefa', 'add')
 
 function creatItemList() {
-    const catchimput = document.querySelector('#texto-tarefa')
-    const catchList = document.querySelector('#lista-tarefas')
-    const catchButton = document.querySelector('#criar-tarefa')
-    catchButton.addEventListener('click', () => {
-        let creatLi = document.createElement('li')
-        creatLi.classList.add('item')
-        catchList.appendChild(creatLi)
-        creatLi.innerHTML = catchimput.value
-        catchimput.value = ''
-    })
+  const catchimput = document.querySelector('#texto-tarefa')
+  const catchList = document.querySelector('#lista-tarefas')
+  const catchButton = document.querySelector('#criar-tarefa')
+  catchButton.addEventListener('click', () => {
+    let creatLi = document.createElement('li')
+    creatLi.classList.add('item')
+    catchList.appendChild(creatLi)
+    creatLi.innerHTML = catchimput.value
+    catchimput.value = ''
+  })
 
 }
 creatItemList()
 
 function selectLi() {
-    const catchOl = document.querySelector('#lista-tarefas')
-    catchOl.addEventListener('click', (event) => {
-        removeSelect()
-        event.target.classList.add('selected')
+  const catchOl = document.querySelector('#lista-tarefas')
+  catchOl.addEventListener('click', (event) => {
+    removeSelect()
+    event.target.classList.add('selected')
 
-    })
+  })
 }
 selectLi()
 
 function removeSelect() {
-    const catchLi = document.querySelectorAll('.item')
-    for (let index = 0; index < catchLi.length; index += 1) {
-        catchLi[index].classList.remove('selected');
-    }
+  const catchLi = document.querySelectorAll('.item')
+  for (let index = 0; index < catchLi.length; index += 1) {
+    catchLi[index].classList.remove('selected');
+  }
 }
 function completedLi() {
-    const catchOl = document.querySelector('#lista-tarefas')
-    catchOl.addEventListener('dblclick', (event) => {
-        if (event.target.classList == 'item' || event.target.classList == 'item selected') {
-            event.target.classList.add('completed')
-        }
-        else {
-            event.target.classList.remove('completed')
-        }
-    })
+  const catchOl = document.querySelector('#lista-tarefas')
+  catchOl.addEventListener('dblclick', (event) => {
+    if (event.target.classList == 'item' || event.target.classList == 'item selected') {
+      event.target.classList.add('completed')
+    }
+    else {
+      event.target.classList.remove('completed')
+    }
+  })
 
 }
 completedLi()
@@ -94,23 +94,23 @@ completedLi()
 buttons('apaga-tudo', 'apagar')
 /* para remover de forma dinamica, usei essa dica https://pt.stackoverflow.com/questions/441373/como-remover-todos-os-elementos-de-uma-div-em-javascript */
 function buttonRemove() {
-    const catchOl = document.querySelector('#lista-tarefas')
-    const catchButtonRemove = document.querySelector('#apaga-tudo')
-    catchButtonRemove.addEventListener('click', () => {
-        catchOl.innerHTML = ''
-    })
+  const catchOl = document.querySelector('#lista-tarefas')
+  const catchButtonRemove = document.querySelector('#apaga-tudo')
+  catchButtonRemove.addEventListener('click', () => {
+    catchOl.innerHTML = ''
+  })
 }
 buttonRemove()
 
 buttons('remover-finalizados', 'apagar finalizados')
 /* essa função while eu peguei com base no repositorio do gabriel rodrigues https://github.com/tryber/sd-016-b-project-todo-list/pull/78/commits/81c677b62f4d88614106cd4f367d971583029898 */
 function buttonRemoveCompleted() {
-    const catchButtonRemoveCompleted = document.querySelector('#remover-finalizados')
-    catchButtonRemoveCompleted.addEventListener('click', () => {
-        while (document.querySelector('.completed')) {
-            document.querySelector('.completed').remove();
-        }
-    })
+  const catchButtonRemoveCompleted = document.querySelector('#remover-finalizados')
+  catchButtonRemoveCompleted.addEventListener('click', () => {
+    while (document.querySelector('.completed')) {
+      document.querySelector('.completed').remove();
+    }
+  })
 }
 buttonRemoveCompleted()
 
@@ -122,11 +122,40 @@ function saveList() {
 buttons('remover-selecionado', 'remover selecionados')
 
 function removeSelected() {
-    const catchButtonRemoveSelected = document.querySelector('#remover-selecionado')
-    catchButtonRemoveSelected.addEventListener('click', () => {
+  const catchButtonRemoveSelected = document.querySelector('#remover-selecionado')
+  catchButtonRemoveSelected.addEventListener('click', () => {
     document.querySelector('.selected').remove();
-        
-    })
+
+  })
 
 }
 removeSelected()
+
+buttons('mover-cima', 'up')
+/*  dica do lucas araujo para mover https://stackoverflow.com/questions/34913953/move-an-element-one-place-up-or-down-in-the-dom-tree-with-javascript */
+function moveUp() {
+  let catchButtonUp = document.querySelector('#mover-cima')
+  catchButtonUp.addEventListener('click', () => {
+    let catchItem = document.querySelector('.selected')
+    if (catchItem.previousElementSibling) {
+      catchItem.parentNode.insertBefore(catchItem, catchItem.previousElementSibling)
+    }
+   
+  })
+}
+moveUp()
+
+buttons('mover-baixo', 'down')
+/* nextElementSibling : retorna o próximo elemento.
+previousElementSibling : retorna o elemento anterior */
+function moveDown() {
+  let catchButtonDown = document.querySelector('#mover-baixo')
+  catchButtonDown.addEventListener('click', () => {
+    let catchItem = document.querySelector('.selected')
+    if (catchItem.nextElementSibling) {
+      catchItem.parentNode.insertBefore(catchItem.nextElementSibling, catchItem)
+    }
+   
+  })
+}
+moveDown()
