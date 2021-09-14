affc6a844fc32dbed135f64c03c765a595e2b0f0
Taina <tainnaps@gmail.com>
2021-09-14 18:31:15 -0300

    Requisito 10 concluido

--- script.js
+++ script.js
@@ -33,23 +33,23 @@ addElementToDOM(header, title);
 const paragraph = creatElementHTML('p');
 setId(paragraph, 'funcionamento');
 setInnerText(paragraph, 'Clique duas vezes em um item para marcá-lo como completo');
-addElementToDOM(header, paragraph);
+addElementToDOM(main, paragraph);
 
 const input = creatElementHTML('input');
 setId(input, 'texto-tarefa');
 input.type = 'text';
 addElementToDOM(main, input);
 
-const button = creatElementHTML('button');
-setId(button, 'criar-tarefa');
-setInnerText(button, 'Criar');
-addElementToDOM(main, button);
+const createButton = creatElementHTML('button');
+setId(createButton, 'criar-tarefa');
+setInnerText(createButton, 'Criar');
+addElementToDOM(main, createButton);
 
 const taskList = creatElementHTML('ol');
 setId(taskList, 'lista-tarefas');
 addElementToDOM(main, taskList);
 
-let tasks = [];
+// let tasks = [];
 
 function changeSelectedTask(selectedTask, newTask) {
   const backgroundColor = 'gray-background';
@@ -59,7 +59,7 @@ function changeSelectedTask(selectedTask, newTask) {
   }
 }
 
-function changeTaskBackgroundColor() {
+function changeTaskBackgroundColor(tasks) {
   for (let index = 0; index < tasks.length; index += 1) {
     tasks[index].addEventListener('click', (event) => {
       const selectedTask = document.querySelector('.gray-background');
@@ -73,6 +73,20 @@ function changeTaskBackgroundColor() {
   }
 }
 
+function markAsCompleted(tasks) {
+  for (let index = 0; index < tasks.length; index += 1) {
+    tasks[index].addEventListener('dblclick', (event) => {
+      event.target.classList.toggle('completed');
+    });
+  }
+
+  /* tasks.forEach((element) => {
+    element.addEventListener('dblclick', () => {
+      element.classList.add('completed');
+    });
+  }); */
+}
+
 function createTask() {
   if (input.value !== '') {
     const task = creatElementHTML('li');
@@ -83,8 +97,21 @@ function createTask() {
   } else {
     alert('Ops! Você ainda não inseriu a tarefa.');
   }
-  tasks = document.getElementsByClassName('task');
-  changeTaskBackgroundColor();
+  const tasks = document.getElementsByClassName('task');
+  changeTaskBackgroundColor(tasks);
+  markAsCompleted(tasks);
 }
 
-button.addEventListener('click', createTask);
+createButton.addEventListener('click', createTask);
+
+const clearButton = creatElementHTML('button');
+setId(clearButton, 'apaga-tudo');
+setInnerText(clearButton, 'Apagar lista');
+addElementToDOM(main, clearButton);
+
+clearButton.addEventListener('click', () => {
+  // ref: https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+});
