24f4d3c93c051fba8079a11fb5080c32c8db16a9
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-13 21:58:00 -0300

    Adicionado requisito 2

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const body = document.getElementsByTagName('body')[0];
 
 function addMain() {
-  let main = document.createElement('main');
+  const main = document.createElement('main');
   main.id = 'main';
   body.appendChild(main);
 }
-
 addMain();
 const main = document.getElementById('main');
 
 // REQUISITO 1
 function addHeader() {
-  let header = document.createElement('header');
+  const header = document.createElement('header');
   header.innerHTML = '<h1>Minha Lista de Tarefas</h1>';
   main.appendChild(header);
 }
-
 addHeader();
+
+// REQUISITO 2
+function addFuncionamento() {
+  const newParagraph = document.createElement('p');
+  newParagraph.id = 'funcionamento';
+  newParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+  main.appendChild(newParagraph);
+}
+addFuncionamento();
