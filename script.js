d605df2ae9307aa3f5319741e4a9bec579c79601
Seu nome <senamatheus.job@gmail.com>
2021-09-14 16:03:05 -0300

    Falta finalizar somente requisito 8

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-let getAddField = document.querySelector('#texto-tarefa');
-let getAddButton = document.querySelector('#criar-tarefa');
-let getTaskList = document.querySelector('#lista-tarefas');
+function createTask(){
 
-
+const getAddField = document.querySelector('#texto-tarefa');
+const getAddButton = document.querySelector('#criar-tarefa');
+const getTaskList = document.querySelector('#lista-tarefas');
+const delAllBtn = document.querySelector('#apaga-tudo')
+const delDone = document.querySelector('#remover-finalizados')
 
 
 getAddButton.addEventListener('click', function() {
   if (getAddField.value !== '') {
     let newLi = document.createElement('li');
+    
     newLi.className = 'item-list';
     newLi.innerText = getAddField.value;
-    let itens = document.getElementsByClassName('item-list')
-
+        
     getTaskList.appendChild(newLi);
     getAddField.value = '';
 
-    for(let i = 0; i < itens.length; i += 1){
-      itens[i].addEventListener('click', function(){
-      itens[i].style.backgroundColor = 'rgb(128, 128, 128)'
-      })
-    
-
-    }
-        
   }     
 })  
 
+getTaskList.addEventListener('click',function(e){
+  let click = e.target;
+  click.style.backgroundColor = 'rgb(128,128,128)'
+})
 
 
+getTaskList.addEventListener('dblclick', function(evt){
+  let clicado = evt.target;
+
+  if(clicado.classList.contains("completed")){
+    clicado.classList.remove("completed")
+  } else {
+    clicado.classList.add("completed")
+  }
+
+})
+
+delAllBtn.addEventListener('click', function(){
+  getTaskList.innerText = '';
+})
+
+
+delDone.addEventListener('click',function(){
+  const finalizados = document.querySelectorAll('.completed')
+  
+  for(let i = 0; i < finalizados.length; i += 1){
+    if(finalizados[i].classList.contains("completed")){
+      getTaskList.removeChild(finalizados[i])
+    }
+  }
+
+
+})
+
+
+}
+
+
+createTask();
