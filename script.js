5663194e1f8bd4291a52f4b3422b90a9cbfaa6c0
MychaelJales <mychael_duarte@hotmail.com>
2021-09-13 21:59:16 -0300

    Finalizando os itens 10 e 11

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const addTarefa = document.getElementById('criar-tarefa');
 
 const todoList = document.getElementById('lista-tarefas');
 
+const apagaTudo = document.getElementById('apaga-tudo');
+
+const removerFinalizadas = document.getElementById('remover-finalizados');
+
+const classSelect = 'item-selected';
+
 function addTarefaFunction() {
   const createLi = document.createElement('li');
 
@@ -17,16 +23,19 @@ function addTarefaFunction() {
 }
 
 function checkClass() {
-  const arrayClassList = document.getElementsByClassName('item-selected');
+  const arrayClassList = document.getElementsByClassName(classSelect);
+
   for (let index = 0; index < arrayClassList.length; index += 1) {
-    arrayClassList[index].classList.remove('item-selected');
+    arrayClassList[index].classList.remove(classSelect);
   }
 }
 
 function selectedItem(event) {
   checkClass();
+
   const clickItem = event.target;
-  clickItem.classList.add('item-selected');
+
+  clickItem.classList.add(classSelect);
 }
 
 function todoComplete(event) {
@@ -35,8 +44,28 @@ function todoComplete(event) {
   doubleClickItem.toggle('completed');
 }
 
+function apagarTudo() {
+  const selectLis = document.querySelector('ol');
+
+  while (selectLis.firstChild) {
+    selectLis.removeChild(selectLis.firstChild);
+  }
+}
+
+function removeFinalizadas() {
+  const completeList = document.getElementsByClassName('completed');
+  const lengthList = completeList.length;
+  for (let index = 0; index < lengthList; index += 1) {
+    completeList[0].remove();
+  }
+}
+
 addTarefa.addEventListener('click', addTarefaFunction);
 
 todoList.addEventListener('click', selectedItem);
 
 todoList.addEventListener('dblclick', todoComplete);
+
+apagaTudo.addEventListener('click', apagarTudo);
+
+removerFinalizadas.addEventListener('click', removeFinalizadas);
