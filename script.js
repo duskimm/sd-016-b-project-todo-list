f713352681dd96736d927ffe038efef4ee7605c0
Breno Tainan <brenotaguiar@gmail.com>
2021-09-13 21:22:13 -0300

    requisito 07

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const textoInput = document.getElementById('texto-tarefa');
 const valorButao = document.getElementById('criar-tarefa');
 const listaDeItens = document.getElementById('lista-tarefas');
+// const itensDaLista = document.getElementsByClassName('.itemLista');
 
 valorButao.addEventListener('click', () => {
   const elementoLista = document.createElement('li');
   const texto = textoInput.value;
   listaDeItens.appendChild(elementoLista);
   elementoLista.innerText = texto;
+  elementoLista.className = 'itemLista';
   textoInput.value = '';
+});
+
+listaDeItens.addEventListener('click', (event) => {
+  event.target.classList.toggle('corCinza');
 });
