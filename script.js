0ba8bc1c9c1afc6b05586ab3ee8cf440194a31d2
Giovanni Greco <giovannivpgreco@protonmail.com>
2021-09-14 15:20:38 -0300

    Requisitos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function addTask() {
   let taskList = document.getElementById('lista-tarefas');
   let newTask = document.createElement('li');
   let taskContent = document.getElementById('texto-tarefa').value;
+  newTask.addEventListener('click', highlight);
+  newTask.addEventListener('dblclick', lineThrough);
 
   taskList.appendChild(newTask);
   newTask.innerText = taskContent;
@@ -13,11 +15,33 @@ document.getElementById('criar-tarefa').addEventListener('click', addTask);
 
 // Requisito 7 e 8
 
-// 
-function clearList(){
+function highlight() {
+  const currentTasks = document.getElementsByTagName('li')
+  for (let i = 0; i < currentTasks.length; i += 1) {
+    if (currentTasks[i] == 'rgb(128, 128, 128)') {
+      currentTasks[i].style.backgroundColor = 'white';
+    }  
+  }
+  this.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+// Requisito 9
+function lineThrough() {
+  this.classList.add('.completed');
+}
+
+// Requisito 10
+function clearList() {
   let currentList = document.querySelectorAll('li')
   for (let i in currentList) {
       currentList[i].remove()
   }
 }
 document.getElementById('apaga-tudo').addEventListener('click', clearList);
+
+// Requisito 12
+
+function saveList() {
+    
+}
+document.getElementById('salvar-tarefas').addEventListener('click', saveList)
