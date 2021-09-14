5d50aa42d5c3b8e73d5e286adfe8e64609ce0c51
Marcio Junior <marcio.mj70@gmail.com>
2021-09-14 16:15:14 -0300

    select one element of list

--- script.js
+++ script.js
@@ -2,34 +2,40 @@ const taskInput = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const addTaskBtn = document.getElementById('criar-tarefa');
 
-function addBackgroundToTask(tasks) {
-  let taskState = {
-    selected: false,
-  };
-  tasks.forEach((task) => {
-    task.addEventListener('click', (event) => {
-      const selectedTask = event.target;
-
-      if (!taskState.selected) {
-        selectedTask.style.backgroundColor = 'gray';
-      } else {
-        selectedTask.style.backgroundColor = 'white';
-      }
-
-      taskState = {
-        selected: !taskState.selected,
-      };
-    });
+function resetSelected() {
+  const AllTaks = document.querySelectorAll('.task-item');
+  AllTaks.forEach((oneTask) => {
+    const task = oneTask;
+    task.classList.remove('selected');
+    task.style.backgroundColor = 'white';
   });
 }
 
-addTaskBtn.addEventListener('click', () => {
-  const taskElement = document.createElement('li');
-  taskElement.innerText = taskInput.value;
-  taskElement.classList.add('task-item');
-  taskList.appendChild(taskElement);
-  taskInput.value = '';
+function addBackgroundToTask(tasks) {
+  tasks.forEach((task) => {
+    task.addEventListener('click', (event) => {
+      const selectedTask = event.target;
+      resetSelected();
+      selectedTask.classList.add('selected');
+      if (selectedTask.classList.contains('selected')) {
+        selectedTask.style.backgroundColor = 'gray';
+      }
+    });
+  });
+}
+function AddTask(buttonElement) {
+  buttonElement.addEventListener('click', () => {
+    const taskElement = document.createElement('li');
+    taskElement.innerText = taskInput.value;
+    taskElement.classList.add('task-item');
+    taskList.appendChild(taskElement);
+    taskInput.value = '';
 
-  const tasks = document.querySelectorAll('.task-item');
-  addBackgroundToTask(tasks);
-});
+    const tasks = document.querySelectorAll('.task-item');
+    addBackgroundToTask(tasks);
+  });
+}
+
+window.onload = () => {
+  AddTask(addTaskBtn);
+};
