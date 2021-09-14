6444cb29d7424843b18a058058c7bef6528f9af3
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2021-09-14 17:14:16 -0300

    Primeiras alteracoes do lint e style

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 // Constantes
-const task = document.querySelector("#texto-tarefa");
-const newTask = document.querySelector("#criar-tarefa");
-const list = document.querySelector("#lista-tarefas");
-const clenear = document.querySelector("#apaga-tudo");
-const clearCompletedTask = document.querySelector("#remover-finalizados");
-const clearSelected = document.querySelector("#remover-selecionado");
-const saveButton = document.querySelector("#salvar-tarefas");
+const task = document.querySelector('#texto-tarefa');
+const newTask = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
+const clenear = document.querySelector('#apaga-tudo');
+const clearCompletedTask = document.querySelector('#remover-finalizados');
+const clearSelected = document.querySelector('#remover-selecionado');
+const saveButton = document.querySelector('#salvar-tarefas');
 const up = document.getElementById('mover-cima');
 const down = document.getElementById('mover-baixo');
 
 // add a new task
 function addTask() {
-  const newLi = document.createElement("li");
+  const newLi = document.createElement('li');
   const text = task.value;
   newLi.innerText = text;
   list.appendChild(newLi);
-  task.value = "";
-};
-newTask.addEventListener("click", addTask);
+  task.value = '';
+}
+newTask.addEventListener('click', addTask);
 // BackGroundColor change
 function selectedItem(event) {
-  const allTasks = document.querySelectorAll("li");
-  if (event.target.classList.contains("selected")) {
-    event.target.classList.remove("selected");
+  const allTasks = document.querySelectorAll('li');
+  if (event.target.classList.contains('selected')) {
+    event.target.classList.remove('selected');
   } else {
     for (let index = 0; index < allTasks.length; index += 1) {
-      allTasks[index].classList.remove("selected");
+      allTasks[index].classList.remove('selected');
     }
-      event.target.classList.add("selected");
+    event.target.classList.add('selected');
   }
-};
-list.addEventListener("click", selectedItem);
+}
+list.addEventListener('click', selectedItem);
 // Completed tasks
 function completedTask(event) {
-  if (event.target.classList.contains("completed")){
-    event.target.classList.remove("completed");
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
   } else {
-    event.target.classList.add("completed");
+    event.target.classList.add('completed');
   }
-};
-list.addEventListener("dblclick", completedTask);
+}
+list.addEventListener('dblclick', completedTask);
 // Clear List
 // Para realizar este requisito busquei auxilio no site stackoverflow e MDN nos
 // seguintes links.
@@ -50,27 +50,27 @@ function clearList() {
   while (list.firstChild) {
     list.firstChild.remove();
   }
-};
-clenear.addEventListener("click", clearList);
+}
+clenear.addEventListener('click', clearList);
 // Clear Completed tasks
 function clearCompletedTasks() {
-  const allTasksCompleted = document.querySelectorAll("#lista-tarefas .completed");
+  const allTasksCompleted = document.querySelectorAll('#lista-tarefas .completed');
   for (let index = 0; index < allTasksCompleted.length; index += 1) {
     allTasksCompleted[index].remove();
   }
-};
-clearCompletedTask.addEventListener("click", clearCompletedTasks);
+}
+clearCompletedTask.addEventListener('click', clearCompletedTasks);
 // Clear the selected task
 function clearSelectedTask() {
-  const allTasks = document.querySelectorAll("li");
-  const selectedTask = document.querySelector(".selected");
+  const allTasks = document.querySelectorAll('li');
+  const selectedTask = document.querySelector('.selected');
   for (let index = 0; index < allTasks.length; index += 1) {
-    if ( allTasks[index] === selectedTask) {
+    if (allTasks[index] === selectedTask) {
       allTasks[index].remove();
     }
   }
-};
-clearSelected.addEventListener("click", clearSelectedTask);
+}
+clearSelected.addEventListener('click', clearSelectedTask);
 // Save the List and restore
 // Para realizar esse requisito eu busquei informações nos seguintes sites.
 // link:https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
@@ -81,44 +81,43 @@ function saveList() {
   window.localStorage.clear();
   const tasksToSave = [];
   const tasksClassToSave = [];
-  const allTasks = document.querySelectorAll("#lista-tarefas li");
+  const allTasks = document.querySelectorAll('#lista-tarefas li');
   for (let index = 0; index < allTasks.length; index += 1) {
     tasksClassToSave.push(allTasks[index].className);
     tasksToSave.push(allTasks[index].innerText);
   }
-  window.localStorage.setItem("TaskStore", JSON.stringify(tasksToSave));
-  window.localStorage.setItem("TaskClassStore", JSON.stringify(tasksClassToSave));
-};
-saveButton.addEventListener("click", saveList);
-  
+  window.localStorage.setItem('TaskStore', JSON.stringify(tasksToSave));
+  window.localStorage.setItem('TaskClassStore', JSON.stringify(tasksClassToSave));
+}
+saveButton.addEventListener('click', saveList);
 function restoreList() {
   const savedList = JSON.parse(localStorage.TaskStore).length;
   for (let index = 0; index < savedList; index += 1) {
-    const newLi = document.createElement("li");
-    let text = JSON.parse(window.localStorage.TaskStore)[index];
-    let name = JSON.parse(window.localStorage.TaskClassStore)[index];
+    const newLi = document.createElement('li');
+    const text = JSON.parse(window.localStorage.TaskStore)[index];
+    const name = JSON.parse(window.localStorage.TaskClassStore)[index];
     newLi.innerText = text;
     newLi.className = name;
     list.appendChild(newLi);
   }
-};
+}
 
 // Precisei buscar informações no seguinte site.
 // link : https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore
 function goUp() {
   const selected = document.querySelector('.selected');
-  return list.insertBefore(selected, selected.previousElementSibling);
+  list.insertBefore(selected, selected.previousElementSibling);
 }
-up.addEventListener("click", goUp);
-  
+up.addEventListener('click', goUp);
+
 function goDown() {
   const selected = document.querySelector('.selected');
-  return list.insertBefore(selected, selected.nextElementSibling.nextElementSibling);
+  list.insertBefore(selected.nextElementSibling, selected);
 }
-down.addEventListener("click", goDown);
+down.addEventListener('click', goDown);
 
-window.onload = function() {
+window.onload = function carregada() {
   if (localStorage.TaskStore) {
     restoreList();
-  };
+  }
 };
