927b58d1725378b7d9f94ac69d03127a4de8f327
blmarquess <blmarques.dev@gmail.com>
2021-09-14 16:04:35 -0300

    select 'n includ selection class done

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+// cmds
+
+function selectTask(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  }
+  event.target.classList.add('completed');
+}
+
+function taskComplet() {
+  console.log('Click dublo marca como completo!');
+}
+
 function newTesc() {
   const imputNode = document.querySelector('#texto-tarefa');
   const location = document.querySelector('#lista-tarefas');
   const newTsc = document.createElement('li');
   newTsc.innerText = imputNode.value;
   location.appendChild(newTsc);
+  newTsc.addEventListener('click', selectTask);
+  newTsc.addEventListener('dblclick', taskComplet);
   imputNode.value = '';
 }
 
 function inputTest() {
   const test = document.querySelector('#texto-tarefa');
-  if (test === '') {
+  if (test === 'undefined') {
     return alert('Insira uma tarefa por gentileza!');
   }
   newTesc();
 }
 
 const btnCriar = document.querySelector('#criar-tarefa');
-btnCriar.addEventListener('click', newTesc);
+btnCriar.addEventListener('click', inputTest);
