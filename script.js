1b4ca81852b96e6419d46f5f033b811acd832590
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-14 19:03:32 -0300

    C11 Requisito Bonus 12 - LocalStorage

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const listaTarefa = document.querySelector('#lista-tarefas');
+const bgGraySelector = document.getElementsByClassName('bgCinza');
 
 function criarTarefa() {
   const inputText = document.querySelector('#texto-tarefa').value;
@@ -19,10 +20,9 @@ function completedClass(evento) {
 
 function bgGray(event) {
   const eventTarget = event.target;
-  const bgGraySelector = document.getElementsByClassName('bgCinza');
   if (eventTarget !== listaTarefa) {
     if (bgGraySelector.length > 0) {
-      bgGraySelector[0].style.backgroundColor = '#e6e4db';
+      bgGraySelector[0].style.backgroundColor = 'rgb(230, 228, 219)';
       bgGraySelector[0].classList.remove('bgCinza');
       eventTarget.classList.add('bgCinza');
       bgGraySelector[0].style.backgroundColor = 'rgb(128, 128, 128';
@@ -45,6 +45,37 @@ function removeCompleted() {
   }
 }
 
+function saveTasks() {
+  localStorage.clear();
+  const elemTagLi = document.getElementsByTagName('li');
+  const finalLi = elemTagLi.length;
+  let listaLi = {};
+  for (let i = 0; i < finalLi; i += 1) {
+    let classLi = '';
+    const elemIter = elemTagLi[i];
+    const strEle = elemIter.innerText;
+    const clSel = elemIter.classList.contains('completed');
+    if (clSel) {
+      classLi = 'completed';
+    }
+    listaLi = { str: strEle, classs: classLi };
+    localStorage.setItem(`li${i}`, JSON.stringify(listaLi));
+  }
+}
+
+function setInicial() {
+  const localSIze = localStorage.length;
+  for (let i = 0; i < localSIze; i += 1) {
+    const actSelec = JSON.parse(localStorage.getItem(`li${i}`));
+    const newLi2 = document.createElement('li');
+    newLi2.innerText = actSelec.str;
+    if (actSelec.classs) {
+      newLi2.className = actSelec.classs;
+    }
+    listaTarefa.appendChild(newLi2);
+  }
+}
+
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 btnCriarTarefa.addEventListener('click', criarTarefa);
 const btnReset = document.querySelector('#apaga-tudo');
@@ -54,4 +85,6 @@ olLi2.addEventListener('dblclick', completedClass);
 btnReset.addEventListener('click', resetAll);
 const btnRemoveCom = document.querySelector('#remover-finalizados');
 btnRemoveCom.addEventListener('click', removeCompleted);
-// se estiver selecionado, deselecione
+const btnSaveTasks = document.querySelector('#salvar-tarefas');
+btnSaveTasks.addEventListener('click', saveTasks);
+window.onload = setInicial;
