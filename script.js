f9184041e56c5c749f2118539f332a60017d374c
Arthur Silva <arthur1silva1r@gmail.com>
2021-09-14 16:07:08 -0300

    requisito 12

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const btnCleanComp = document.getElementById('remover-finalizados');
 const btnUp = document.getElementById('mover-cima');
 const btnDown = document.getElementById('mover-baixo');
 const btnRemove = document.getElementById('remover-selecionado');
+const btnSave = document.getElementById('salvar-tarefas');
+
+for (let i = 0; i < localStorage.length; i += 1) {
+  const task = document.createElement('li');
+  console.log(localStorage.getItem(`${i}`));
+  const arrayClass = localStorage.getItem(`${i}`).split(',');
+  task.innerText = `${arrayClass[0]}`;
+  task.classList.add(`${arrayClass[1]}`);
+  olTasks.appendChild(task);
+}
 
 function criaTarefa() {
   const newTask = document.createElement('li');
@@ -77,5 +87,23 @@ btnRemove.addEventListener('click', () => {
     if (arrayLi[i].classList.contains('selected')) {
       olTasks.removeChild(arrayLi[i]);
     }
+  }
+});
+
+function addStorage() {
+  for (let i = 0; i < olTasks.children.length; i += 1) {
+    if (olTasks.children[i].classList.contains('completed')) {
+      localStorage.setItem(`${i}`, `${olTasks.children[i].innerText},completed`);
+    } else {
+      localStorage.setItem(`${i}`, `${olTasks.children[i].innerText}`);
+    }
+  }
+}
+
+btnSave.addEventListener('click', () => {
+  if (olTasks.children.length === 0) {
+    localStorage.clear();
+  } else {
+    addStorage();
   }
 });
