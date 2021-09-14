6aa27c0746452f1662e0ccf5cc10d0e6454a0840
Bruno Melo <melobrunoti@gmail.com>
2021-09-14 10:31:16 -0300

    Requisitos completos

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let lista = document.querySelector('#Lista-tarefas');
-let item = document.getElementById('texto-tarefa');
-let createButton = document.getElementById('criar-tarefa')
-let items = document.getElementsByClassName('list-itens')
-let clear = document.getElementById('apaga-tudo')
-let getCompletedButton = document.getElementById('remover-finalizados')
+const lista = document.querySelector('#Lista-tarefas');
+const item = document.getElementById('texto-tarefa');
+const createButton = document.getElementById('criar-tarefa');
+const items = document.getElementsByClassName('list-itens');
+const clear = document.getElementById('apaga-tudo');
+const getCompletedButton = document.getElementById('remover-finalizados');
+const getSaveListButton = document.getElementById('salvar-tarefas');
+const getMoveUpButton = document.getElementById('mover-cima');
+const getMoveDownButton = document.getElementById('mover-baixo');
+const getRemoveButton = document.getElementById('remover-selecionado');
+
 function selectItem(event) {
   for (let i = 0; i < items.length; i += 1) {
     items[i].classList.remove('selected');
@@ -11,38 +16,87 @@ function selectItem(event) {
   event.target.classList.add('selected');
 }
 
-function createTask(){
-  let Li_item = document.createElement('li')
-  itemValue = item.value
-  Li_item.innerText = itemValue
-  Li_item.classList.add('list-itens')
-  lista.appendChild(Li_item)
+function createTask() {
+  const liItem = document.createElement('li');
+  const itemValue = item.value;
+  liItem.innerText = itemValue;
+  liItem.classList.add('list-itens');
+  lista.appendChild(liItem);
   item.value = '';
-  Li_item.addEventListener('click',selectItem)
+  liItem.addEventListener('click', selectItem);
 }
 
-createButton.addEventListener('click', createTask)
-
-
+createButton.addEventListener('click', createTask);
 
 // metodo toggle https://www.horadecodar.com.br/2020/10/08/detectar-se-um-elemento-contem-uma-classe-com-javascript/
-function completedItem(event){
-  event.target.classList.toggle('completed')
-  }
-lista.addEventListener('dblclick',completedItem)
+function completedItem(event) {
+  event.target.classList.toggle('completed');
+}
+lista.addEventListener('dblclick', completedItem);
 
-
-function removeAll(){
-    while (lista.firstChild) {
+function removeAll() {
+  while (lista.firstChild) {
     lista.removeChild(lista.lastChild);
   }
 }
-clear.addEventListener('click',removeAll);
+clear.addEventListener('click', removeAll);
 
-function removeCompleted(){
-  let getCompleted = document.getElementsByClassName('completed')
- while (getCompleted.length > 0) {
-   lista.removeChild(getCompleted[0])
- }
+function removeCompleted() {
+  const getCompleted = document.getElementsByClassName('completed');
+  while (getCompleted.length > 0) {
+    lista.removeChild(getCompleted[0]);
+  }
 }
-getCompletedButton.addEventListener('click', removeCompleted)
+getCompletedButton.addEventListener('click', removeCompleted);
+
+function save() {
+  const savedList = lista.innerHTML;
+
+  localStorage.setItem('saved-list', savedList);
+  alert('Lista Salva!');
+}
+getSaveListButton.addEventListener('click', save);
+
+window.onload = function storage() {
+  lista.innerHTML = localStorage.getItem('saved-list');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener('click', selectItem);
+  }
+};
+function mooveUP() {
+  const selectedItem = document.querySelector('.selected');
+
+  if (selectedItem === null) {
+    return alert('Nenhum item selecionado!');
+  }
+  if (selectedItem.previousElementSibling === null
+  || selectedItem.previousElementSibling === undefined) {
+    return alert('O Item já está na primeira posição !');
+  }
+
+  lista.insertBefore(selectedItem, selectedItem.previousElementSibling);
+}
+
+function mooveDown() {
+  const selectedItem = document.querySelector('.selected');
+
+  if (selectedItem == null) {
+    return alert('Nenhum item selecionado!');
+  }
+
+  if (selectedItem.nextElementSibling === null || selectedItem.nextElementSibling === undefined) {
+    return alert('O Item já está na ultima posição !');
+  }
+
+  const mooveDownItem = selectedItem.nextElementSibling.nextElementSibling;
+  lista.insertBefore(selectedItem, mooveDownItem);
+}
+getMoveUpButton.addEventListener('click', mooveUP);
+getMoveDownButton.addEventListener('click', mooveDown);
+
+function removeSelected() {
+  const selectedItem = document.querySelector('.selected');
+  lista.removeChild(selectedItem);
+}
+
+getRemoveButton.addEventListener('click', removeSelected);
