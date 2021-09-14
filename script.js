aaa3cf822de24d9c88fd43089127c19dae68f6a6
blmarquess <blmarques.dev@gmail.com>
2021-09-14 20:13:04 -0300

    Requirement 11 and 14 done and 12 and 13 50%

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ function selectTask(event) {
   event.target.classList.add('selected');
 }
 
+function clearSelected() {
+  const grup = document.querySelectorAll('li');
+  grup.forEach((alvo) => { if (alvo.classList.contains('selected')) alvo.remove(); });
+}
+
+function clearAll() {
+  const allLi = document.querySelector('ol');
+  allLi.innerHTML = '';
+}
+
+function clearDones() {
+  const list = document.querySelectorAll('li');
+  list.forEach((done) => { if (done.classList.contains('completed')) done.remove(); });
+}
+
 function taskComplet(event) {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
@@ -38,5 +53,32 @@ function inputTest() {
   newTesc();
 }
 
+function salvar() {
+  const nodeDB = document.querySelector('ol');
+  const localDB = nodeDB;
+  localStorage.setItem('listDB', localDB);
+  console.log(nodeDB);
+}
+
+function loadDB() {
+  const localDB = localStorage.getItem(loadDB);
+  const node = document.querySelector('ol');
+  node.innerHTML = localDB;
+}
+
 const btnCriar = document.querySelector('#criar-tarefa');
 btnCriar.addEventListener('click', inputTest);
+
+const btnClrAll = document.querySelector('#apaga-tudo');
+btnClrAll.addEventListener('click', clearAll);
+
+const btnClrSelected = document.querySelector('#remover-selecionado');
+btnClrSelected.addEventListener('click', clearSelected);
+
+const btnClrDone = document.querySelector('#remover-finalizados');
+btnClrDone.addEventListener('click', clearDones);
+
+const btnSalve = document.querySelector('#salvar-tarefas');
+btnSalve.addEventListener('click', salvar);
+
+window.onload = () => loadDB();
