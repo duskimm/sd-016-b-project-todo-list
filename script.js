642f8c31f3f4a0080c079131bb419a2a4d6802f7
Lucas <lb.camargo94@gmail.com>
2021-09-15 17:00:29 -0300

    Concluído item 10

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const criarTarefa = document.getElementById('criar-tarefa');
 const textoTarefa = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
+const limpaTudo = document.getElementById('apaga-tudo');
+
 
 function limparCapmo() {
-  textoTarefa.value = "";
+  textoTarefa.value = '';
 }
 
 function adicionarTarefa() {
@@ -12,6 +14,7 @@ function adicionarTarefa() {
     const novaTarefa = document.createElement('p');
     novoItem.classList.add('item-lista-tarefas');
     novaTarefa.classList.add('nova-tarefa');
+
     listaTarefas.appendChild(novoItem);
     novoItem.appendChild(novaTarefa);
     novaTarefa.innerHTML = textoTarefa.value;
@@ -26,13 +29,20 @@ function removerTarefa() {
     listaTarefas.removeChild(ultimoItem);
   }
 }
-// Para remover todos os filhos de um elemento:
 
-// let element = document.getElementById("top");
-// while (element.firstChild) {
-//   element.removeChild(element.firstChild);
-// }
+function limparCorDeFundo() {}
 
-// Agora o segredo é você adicionar no campo input text que você quer fazer o efeito a seguinte propriedade e valor:
+function alterarCorDeFundo() {
+  console.log('Yes');
+}
+listaTarefas.addEventListener('click', alterarCorDeFundo());
 
-//  onfocus="this.value='';"
+function apagaTudo() {
+  while (listaTarefas.firstChild) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
+}
+limpaTudo.addEventListener('click', apagaTudo());
+
+
+// console.log('Yes');
