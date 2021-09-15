8a636b43e7195184a73341259d95253fca118c29
Kevin Sena <senakevin50@gmail.com>
2021-09-14 23:01:31 -0300

    reseta as cores das li e deixa apenas uma

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 function q(params) {
   return document.body.querySelector(params);
 }
-/*
+
 function qAll(params) {
   return document.body.querySelectorAll(params);
 }
-*/
+
 function appendToChild(element, tag, text, className) {
   const stuf = document.createElement(tag);
   stuf.innerText = text;
@@ -18,10 +18,19 @@ const criarTarefa = q('#criar-tarefa');
 const ol = q('#lista-tarefas');
 const textoTarefa = q('#texto-tarefa');
 
+// reseta as cores
+function resetColor() {
+  const li = qAll('.tasks');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].style.backgroundColor = 'rgb(229 , 229 , 229)';
+  }
+}
+
 // Ao clicar em um item muda sua cor de fundo
 function changeItemColor() {
   const li = ol.lastChild;
   li.addEventListener('click', () => {
+    resetColor();
     li.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 }
