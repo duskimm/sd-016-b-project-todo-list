314ed7c6286fda75bdf4a1eb6963ef8f83bc5187
brunaumLL <bruno99louzada@gmail.com>
2021-09-14 21:17:24 -0300

    requisitos 7, 9 & 10 feitos

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-function novoItemList() {
-  const buttonList = document.getElementById("criar-tarefa");
-  
-  buttonList.addEventListener('click', function(){
-    const textInput = document.getElementById("texto-tarefa");
-    const novoItem = document.createElement('li');
-    const listaTarefas = document.getElementById("lista-tarefas");  
-    
-    novoItem.innerHTML = textInput.value;
-    novoItem.className = 'itemList';
-    textInput.value = '';
-    listaTarefas.appendChild(novoItem);
-  });
-}
-novoItemList();
+const buttonList = document.getElementById('criar-tarefa');
+const textInput = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const apagador = document.getElementById('apaga-tudo');
 
-function armazenaItem(){
+function aleteraCorDeFundo() {
+  const li = listaTarefas.childNodes;
+  for (let index = 0; index < li.length; index += 1) {
+    if (li[index].className === 'itemStyle') {
+      li[index].className = 'styleItem';
+    } else if (li[index].className === 'styleItem') {
+      li[index].className = 'itemStyle';
+    }
+  }
+}
+
+function tarefaFeita() {
+  const riscaLi = listaTarefas.childNodes;
+  for (let index = 0; index < riscaLi.length; index += 1) {
+    if (riscaLi[index].className !== 'completed') {
+      riscaLi[index].className = 'completed';
+    } else {
+      riscaLi[index].className = 'styleItem';
+    }
+  }
+}
+
+function novoItemList() {
+  const novoItem = document.createElement('li');
+  novoItem.addEventListener('click', aleteraCorDeFundo);
+  novoItem.addEventListener('dblclick', tarefaFeita);
+
+  novoItem.innerHTML = textInput.value;
+  novoItem.className = 'itemStyle';
+  textInput.value = '';
+  listaTarefas.appendChild(novoItem);
+}
+buttonList.addEventListener('click', novoItemList);
+
+function armazenaItem() {
   const listaOl = document.getElementById('lista-tarefas').innerHTML;
   localStorage.setItem('saved', listaOl);
 }
 armazenaItem();
+
+function apagaTudo() {
+  listaTarefas.innerHTML = '';
+}
+
+apagador.addEventListener('click', apagaTudo);
