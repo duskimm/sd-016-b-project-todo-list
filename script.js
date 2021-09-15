e8b708fe88c5a512a974a9823996839145c979a2
Celso Fiorani <celsofiorani@gmail.com>
2021-09-14 21:24:08 -0300

    parte9

--- script.js
+++ script.js
@@ -23,14 +23,27 @@ function addTarefa() {
 }
 btnTarefa.addEventListener('click', addTarefa) // O EVENTO PRECISA SER NO BOTAO
 
-//7 
+//7 e 8
 // alterar a cor de fundo para cinza ao click na lista de item rgb(128,128,128)
 
 function alteraCorfundo(event) {
-    let classeListagem = document.querySelectorAll('.listagem');
+    let classeListagem = document.querySelectorAll('.listagem'); // resgata a LI que recebe a classe listagem
     for (let index = 0; index < classeListagem.length; index+= 1) {
         classeListagem[index].style.background = 'white';
     }
     event.target.style.background = 'gray';
 }
-listaDeTarefas.addEventListener('click',alteraCorfundo)
+listaDeTarefas.addEventListener('click',alteraCorfundo);
+
+// 9 Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. 
+// Deve ser possível desfazer essa ação clicando novamente duas vezes no item
+
+function taskCompleted (event){
+    if (event.target.className === 'completed listagem') { // COMPARAÇÃO
+        event.target.className = 'listagem' // ATRIBUIÇÃO
+    } else {
+        event.target.className = 'completed listagem'
+    }
+}
+listaDeTarefas.addEventListener('dblclick',taskCompleted)
+
