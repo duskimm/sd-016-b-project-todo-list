f85a6e548b2669acc1f2349979ca67d48ea39100
Diego-AS-Mendes <dasm021@msn.com>
2021-09-14 23:01:38 -0300

    Requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const botao = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const listaDeTarefas = document.getElementById('lista-tarefas');
+const botaoApagaAll = document.getElementById('apaga-tudo');
 
-function novaTarefa(tarefa) {
-  if (tarefa.target === botao && input.value !== '') {
+function novaTarefa() {
+  //if (tarefa.target === botao && input.value !== '') {
     const criaTarefa = document.createElement('li');
     criaTarefa.setAttribute('class', 'teste');
     listaDeTarefas.appendChild(criaTarefa).innerHTML = input.value;
     input.value = '';
-  }
+  //}
 }
-document.addEventListener('click', novaTarefa);
 
 const listSelect = 'list-select';
 function addRemoveClass(event) {
@@ -25,4 +25,24 @@ function addRemoveClass(event) {
   }
 }
 
+
+const riscaTarefa = 'completed';
+function riscaCompleto(event) {
+  const confereClass = event.target.classList.contains(riscaTarefa);
+  console.log(confereClass);
+  if (confereClass === false) {
+    event.target.classList.add(riscaTarefa);
+    console.log(event.target);
+  } else {
+    event.target.classList.remove(riscaTarefa);
+  }
+}
+
+function apagaTudo() {
+  listaDeTarefas.innerHTML = '';
+}
+
+botao.addEventListener('click', novaTarefa);
+listaDeTarefas.addEventListener('dblclick', riscaCompleto);
 listaDeTarefas.addEventListener('click', addRemoveClass);
+botaoApagaAll.addEventListener('click', apagaTudo);
