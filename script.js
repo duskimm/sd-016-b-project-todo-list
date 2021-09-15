1d859edda2159b79b0135dc0034ad43277bccbfe
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-14 22:34:48 -0300

    Requisito 13 e 14

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const task = document.getElementsByClassName('tarefa');
 const cleanAll = document.getElementById('apaga-tudo');
 const removeFinishedTask = document.getElementById('remover-finalizados');
 const saveTaskButton = document.getElementById('salvar-tarefas');
+const moveUpButton = document.getElementById('mover-cima');
+const moveDownButton = document.getElementById('mover-baixo');
+const removeSelectedButton = document.getElementById('remover-selecionado');
 
 function deselectAllTasks() {
   for (let index = 0; index < task.length; index += 1) {
@@ -68,6 +71,7 @@ function saveTaskList() {
     if (taskList.children.length === 0) {
       localStorage.clear();
     }
+    localStorage.clear();
     for (let index = 0; index < task.length; index += 1) {
       localStorage.setItem(`task${index}`, task[index].innerHTML);
       localStorage.setItem(`class${index}`, task[index].classList.value);
@@ -99,10 +103,52 @@ function loadTaskList() {
   }
 }
 
+function moveUp() {
+  moveUpButton.addEventListener('click', () => {
+    const selectedTask = document.querySelector('.selected');
+    if (selectedTask !== null) {
+      const taskBefore = selectedTask.previousElementSibling;
+      if (selectedTask.previousElementSibling !== null) {
+        selectedTask.parentNode.insertBefore(selectedTask, taskBefore);
+        selectTask();
+      } else {
+        alert('Chegou ao inicio da lista');
+      }
+    }
+  });
+}
+
+function moveDown() {
+  moveDownButton.addEventListener('click', () => {
+    const selectedTask = document.querySelector('.selected');
+    if (selectedTask !== null) {
+      const taskAfter = selectedTask.nextElementSibling;
+      if (selectedTask.nextElementSibling !== null) {
+        const targetPosition = taskAfter.nextElementSibling;
+        selectedTask.parentNode.insertBefore(selectedTask, targetPosition);
+        selectTask();
+      } else {
+        alert('Chegou ao fim da lista');
+      }
+    }
+  });
+}
+
+function removeSelected() {
+  removeSelectedButton.addEventListener('click', () => {
+    const selectedTask = document.querySelector('.selected');
+    selectedTask.parentNode.removeChild(selectedTask);
+    selectTask();
+  });
+}
+
 window.onload = () => {
   loadTaskList();
   addTask();
   cleanTasks();
   removeFinished();
   saveTaskList();
+  moveUp();
+  moveDown();
+  removeSelected();
 };
