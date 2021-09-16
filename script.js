b21c1ebb7ae7c0ddc99259821bb06c9573eb2ca5
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-16 18:58:02 -0300

    Requisito 9 OK.

--- script.js
+++ script.js
@@ -17,4 +17,10 @@ function addTask() {
   task.innerText = taskText.value; //Substitui o texto visível da li criada pelo valor do input #texto-tarefa.
   toDoList.appendChild(task); //Adiciona a li criada já com o texto do input a lista ordenada #lista-tarefa.
   taskText.value = ""; //Substitui o valor do input para string vazia, ou seja, remove o valor do input.
+
+  task.addEventListener('dblclick', function riskTask() {
+    //Adiciona uma interação com o usuário que a partir do evento clique duplo aciona a funçao riskTask.
+
+    task.classList.toggle('completed'); //Por meio do classList é possível adicionar ou retirar uma classe ao elemento. O método toggle permite que adicione a classe "completed" quando o retorno é false, ou seja, quando essa classe não é um atributo do elemento, e retira esse atributo quando o retorno é true.
+  });
 }
