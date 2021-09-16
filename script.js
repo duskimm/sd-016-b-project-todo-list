2c9c0611d85431ec2fcc46d22eaef55f81e57046
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-16 18:53:50 -0300

    Requisito 7 9

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const criarTarefas = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const textoTarefas = document.getElementById('texto-tarefa');
 const apagaTarefas = document.getElementById('apaga-tudo');
+// const moverCima = document.getElementById('mover-cima');
+// const moverBaixo = document.getElementById('mover-baixo');
+// const remover = document.getElementById('remover-finalizados');
 
 criarTarefas.addEventListener('click', () => {
   const itemLista = document.createElement('li');
@@ -19,3 +22,16 @@ criarTarefas.addEventListener('click', () => {
 apagaTarefas.addEventListener('click', () => {
   listaTarefas.innerHTML = '';
 });
+
+// Ref.: https://www.w3schools.com/howto/howto_js_todolist.asp
+listaTarefas.addEventListener('dblclick', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('completed');
+  }
+}, false);
+
+listaTarefas.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('select');
+  }
+}, false);
