dc586c2ad33db8ff9804e3999302ed7d40251a6a
Everton-Curti <evercurti@gmail.com>
2021-09-15 21:45:55 -0300

    exercicio 8 resolvido

--- script.js
+++ script.js
@@ -26,26 +26,37 @@ capturarSectionOne.appendChild(criarBotaoInput);
 let capturarInput = document.querySelector('#texto-tarefa');
 let capturarBotaoAdicionar = document.querySelector('#criar-tarefa');
 let capturarListaOrdenada = document.querySelector('#lista-tarefas');
+
 capturarBotaoAdicionar.addEventListener('click', function(){
     if (capturarInput.value.length > 0) {
         let criarNovaLinha = document.createElement('li');
         criarNovaLinha.innerHTML = capturarInput.value;
+        criarNovaLinha.className = 'cinzou';
         capturarListaOrdenada.appendChild(criarNovaLinha);
         capturarInput.value = '';
+        let capturarNovaLinha = document.querySelectorAll('li');
+        capturarNovaLinha[0].classList.add('selector');               
     } else {
-        alert('Nenhum elemento foi adicionado!!!');        
+        alert('Nenhum elemento foi adicionado!!!');     
     }
 });
 
 capturarListaOrdenada.addEventListener('click', function(event){
-    event.target.style.backgroundColor =  'rgb(128, 128, 128)';   
+    let capturarLi = document.querySelectorAll('li');    
+    for(let index = 0; index < capturarLi.length; index += 1){
+        let capturarClasseSelecao = document.querySelector('.selector');
+        capturarClasseSelecao.classList.remove('selector');
+        event.target.classList.add('selector');
+    }
 });
 
-
-
-
-
-
-
-
-
+capturarListaOrdenada.addEventListener('click', function(){
+    let li = document.querySelectorAll('li');
+    for (let index = 0; index < li.length; index += 1){
+        if(li[index].classList.contains('selector')){
+            li[index].style.backgroundColor = 'rgb(128,128,128)';
+        } else {
+            li[index].style.backgroundColor = 'white';
+        }
+    }    
+});
