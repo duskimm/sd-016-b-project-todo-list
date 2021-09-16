2bfd1a5a87bf351d0c476d8c67de4095b472c18e
Calixto <wellington1081@gmail.com>
2021-09-16 16:19:23 -0300

    requisito 8

--- script.js
+++ script.js
@@ -45,30 +45,59 @@ document.getElementById('criar-tarefa').addEventListener("click", function() {
   let input = document.getElementById("texto-tarefa").value;
   
   let li = document.createElement('li')
-  li.innerText = input
   li.id = 'lista'
+  li.innerText = input
   ol.appendChild(li)
   
  
   document.getElementById("texto-tarefa").value = '';
  
-
-  let xyz = document.getElementById('lista-tarefas');
- 
-  for (let i = 0; i < xyz.children.length; i += 1) {
-
-    li.addEventListener('click', function() {
-      li.style.backgroundColor = 'gray'
-      
-      
-    } )
-    
-  }
-  
 })
+  
+  
+  //   for (let i = 0; i < xyz.children.length; i += 1) {
+  //    xyz.children[i].addEventListener('click', function(event) {
+      
+  //     if (xyz.children[i].style.backgroundColor === 'gray'){
+  //       xyz.children[i].style.backgroundColor = 'white'
+  //     }
+  //     else if (xyz.children[i].style.backgroundColor === ''){
+  //     event.xyz.children[i].style.backgroundColor = 'gray'
+  //     }
+      
+  //   //   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  //   // }
+  //   })
+  // }
+
+  
+
+
+document.getElementById('lista-tarefas').addEventListener('click', function(event){   
+  let listArray = document.querySelectorAll("li");
+
+  if(event.target.style.backgroundColor !== 'rgb(128, 128, 128)'){
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      // eventX = event.target;
+                        
+    }
+    else{ 
+        
+    }
+    for(let index1 = 0; index1 < listArray.length; index1++){
+        if(listArray[index1] !== event.target){
+            listArray[index1].style.backgroundColor = 'white';
+            
+        }
+    }
+    
+})   
 
 
 
+// quando o <li> receber o click, a primeira coisa que ele tem que fazer é percorrer todas as <li> criadas verificando se alguma delas tem o backgrond cinza. Se tiver, pintar de branco. Se não, não faz nada.
+// 2. dai depois, ele vai pegar o evento.alvo e pintar o bg.
+// são só esses comandos, mas a ordem deles importa
 
 
- 
+
