9eac1d1cd9754093d49889d991aff12f11459279
Jonatas Passos <jpc080496@gmail.com>
2021-09-16 10:36:28 -0300

    Requisito 7 Finalizado

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-/* eslint-disable no-param-reassign */
-
-/* Adicionando cor nos itens da lista */
-function addColor(event) {
-  event.target.style.color('red');
-}
-
 /* Adicionando item a lista */
 function addItemList() {
   const listOrdem = document.getElementById('lista-tarefas');
   const texto = document.getElementById('texto-tarefa');
   const newItem = document.createElement('li');
-  newItem.addEventListener('click', addColor);
+  /* Adicionando cor nos itens da lista */
+  newItem.addEventListener('click', (event) => {
+    const aux = event;
+    aux.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    console.log(event.target);
+  });
   listOrdem.appendChild(newItem);
   newItem.innerText = texto.value;
   texto.value = '';
