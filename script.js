9d9a62d74e7d9c5e08f9785262f592e0c9a0b522
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-09-15 21:37:53 -0300

    requisito 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const cabecalho = document.createElement('header');
+const h1 = document.createElement('h1');
+
+document.body.appendChild(cabecalho);
+cabecalho.appendChild(h1);
+h1.innerText = 'Minha Lista de Tarefas';
