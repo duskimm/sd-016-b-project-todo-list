7ab0d3b722266cf22891286c4a43221c5b68ce8c
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-09-15 21:40:43 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cabecalho = document.createElement('header');
 const h1 = document.createElement('h1');
+const p = document.createElement('p');
 
 document.body.appendChild(cabecalho);
 cabecalho.appendChild(h1);
 h1.innerText = 'Minha Lista de Tarefas';
+document.body.appendChild(p);
+p.id = 'funcionamento';
+p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
