ef2fdf0a3cc9ee5c73a5f2be90955ca9b475e79c
Jonatas Passos <jpc080496@gmail.com>
2021-09-16 09:21:47 -0300

    Tentando resolver o requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-param-reassign */
+
+/* Adicionando cor nos itens da lista */
+function addColor(event) {
+  event.target.style.color('red');
+}
+
 /* Adicionando item a lista */
 function addItemList() {
   const listOrdem = document.getElementById('lista-tarefas');
   const texto = document.getElementById('texto-tarefa');
   const newItem = document.createElement('li');
+  newItem.addEventListener('click', addColor);
   listOrdem.appendChild(newItem);
   newItem.innerText = texto.value;
   texto.value = '';
