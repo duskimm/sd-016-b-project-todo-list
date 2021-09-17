ec7e4d86ddc29230944bafc82b0d2ed9e812e271
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-17 14:00:43 -0300

    Requisito 11 OK.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Para a parte de JS, foram consultados os materiais de W3Schools, MDN Web Docs, 
+// Course da Trybe, e aulas da Seção 7 "Working with the DOM (Browser HTML Code) in 
+// JavaScript" do curso "JavaScript - The Complete Guide 2021 (Beginner + Advanced)",
+// ministrado por Maximilian Schwarzmüller. 
+
+// Links:
+// https://www.w3schools.com/
+// https://developer.mozilla.org/en-US
+// https://app.betrybe.com/course
+// https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/
+
 window.onload = function () {
 
   const addTaskButton = document.querySelector('#criar-tarefa');
@@ -5,6 +16,9 @@ window.onload = function () {
 
   const removeAllButton = document.querySelector('#apaga-tudo');
   removeAllButton.addEventListener('click', removeAllTasks); // Quando o botao é clicado, a função removeAllTasks é chamada.
+
+  const removeCompletedTaskButton = document.querySelector('#remover-finalizados');
+  removeCompletedTaskButton.addEventListener('click', removeCompletedTasks); // Quando o botao é clicado, a função removeCompletedTasks é chamada.
 
 }
 
@@ -46,5 +60,13 @@ function removeAllTasks() {
   const toDoList = document.getElementById('lista-tarefas');
   while (toDoList.firstElementChild) {
     toDoList.removeChild(toDoList.firstElementChild);
+  }
+}
+
+function removeCompletedTasks() {
+  // Remove todos os elementos com a classe 'completed' da ol. Enquanto houver li com a classe 'completed', o primeiro item será removido.
+  const completedTasks = document.getElementsByClassName('completed');
+  while (completedTasks.length > 0) {
+    completedTasks[0].parentNode.removeChild(completedTasks[0]);
   }
 }
