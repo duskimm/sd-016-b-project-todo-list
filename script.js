149167f3c4c056e62c28e593984ecf711cddb4ee
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-16 22:37:06 -0300

    finaliza requisito 8

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-//quando eu clicar no botão um li será criada
-
 const addTaskButton = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const text = document.getElementById('texto-tarefa');
 
 function addTasks (){
     const newListItem = document.createElement('li');
+    newListItem.addEventListener('click', changeToGrey);
     list.appendChild(newListItem);
     newListItem.innerText = text.value;
     text.value = '';
 }
 addTaskButton.addEventListener('click', addTasks);
+
+function changeToGrey (event){
+    const listItems = document.querySelectorAll('li');
+    for (let index = 0; index < listItems.length; index += 1) {
+        if (listItems[index].style.backgroundColor = 'rgb(128,128,128)') {
+            listItems[index].style.backgroundColor = 'white';
+        }
+    }
+    event.target.style.backgroundColor = 'rgb(128,128,128)'
+    }
