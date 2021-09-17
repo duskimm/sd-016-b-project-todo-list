a6c8f3deb94468bf2fca4dc0122660ab17046c99
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-16 23:48:25 -0300

    mais ajustes linter

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const addTaskButton = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const text = document.getElementById('texto-tarefa');
 
+function changeToGrey(event) {
+  const listItems = document.querySelectorAll('li');
+  for (let index = 0; index < listItems.length; index += 1) {
+    if (listItems[index].style.backgroundColor = 'rgb(128,128,128)') {
+      listItems[index].style.backgroundColor = 'white';
+    }
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function addTasks() {
   const newListItem = document.createElement('li');
   newListItem.addEventListener('click', changeToGrey);
@@ -11,21 +21,11 @@ function addTasks() {
 }
 addTaskButton.addEventListener('click', addTasks);
 
-function changeToGrey (event){
-  const listItems = document.querySelectorAll('li');
-  for (let index = 0; index < listItems.length; index += 1) {
-      if (listItems[index].style.backgroundColor = 'rgb(128,128,128)') {
-          listItems[index].style.backgroundColor = 'white';
-      }
-  }
-  event.target.style.backgroundColor = 'rgb(128,128,128)'
-  }
-
-list.addEventListener('dblclick', lineThrough);
-function lineThrough (event){
+function lineThrough(event) {
   if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
+    event.target.classList.remove('completed');
   } else {
-      event.target.classList.add('completed');
+    event.target.classList.add('completed');
   }
 }
+list.addEventListener('dblclick', lineThrough);
