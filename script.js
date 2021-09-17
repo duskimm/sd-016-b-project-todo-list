fc4686a7227c3d36ec55047fd43e901ec8e59e52
Jonatas Passos <jpc080496@gmail.com>
2021-09-16 21:10:27 -0300

    Requisito 9 concluído

--- script.js
+++ script.js
@@ -8,16 +8,28 @@ function addColor(event) {
   aux.classList.add('colorindo');
 }
 
+/* Riscando os itens da lista */
+function itemScratched(event) {
+  const liRiscado = event.target;
+  if (liRiscado.classList.contains('completed')) {
+    liRiscado.classList.remove('completed');
+  } else {
+    liRiscado.classList.add('completed');
+  }
+}
+
 /* Adicionando item a lista */
 function addItemList() {
   const listOrdem = document.getElementById('lista-tarefas');
   const texto = document.getElementById('texto-tarefa');
   const newItem = document.createElement('li');
   newItem.addEventListener('click', addColor);
+  newItem.addEventListener('dblclick', itemScratched);
   listOrdem.appendChild(newItem);
   newItem.innerText = texto.value;
   texto.value = '';
 }
 
+/* Input */
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addItemList);
