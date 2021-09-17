0a81de782e9924aa6af2700309ebc20644ee893d
Matheus Machado <matheusmdec@hotmail.com>
2021-09-17 20:45:34 -0300

    Requisitos 06, 07 e 08

--- scrit.js
+++ scrit.js
@@ -1,4 +1,4 @@
-//Requisito 05
+//Requisitos 05 e 06
 function setBtnAddTaskEvent() {
     let btn = document.getElementById('criar-tarefa')
     btn.addEventListener('click', addTaskToList)
@@ -9,14 +9,32 @@ function addTaskToList() {
     let list = document.getElementById('lista-tarefas')
     let task = document.createElement('li')
     task.innerText = inputValue
+    task.className = 'task'
     list.appendChild(task)
     input.value = ''
+    task.addEventListener('click', selectTask)
 }
 
+//Requisito 07
+function selectTask(event) {
+    let task = event.target
+    let taskList = document.querySelectorAll('.task')
+    console.log(task);
+    console.log(taskList);
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].innerText !== task.innerText){
+            taskList[i].className = 'task'
+        } else {
+            taskList[i].className = 'task selected'
+        }
+        
+    }
+}   
 
 
 
 
 window.onload = function(){
     setBtnAddTaskEvent()
+    //setTaskSelectEvent()
 }
