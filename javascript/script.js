392657d2d5bd307439bfc6115113183d41842a1c
Savio <s.drksilva@gmail.com>
2021-09-17 15:13:54 -0300

    Finalized actions of remove all elements completed and remove all elements of list

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,13 @@
 let elis = [];
 /**
- * Prototipo de Função;
- * Pega e Retorna Valores;
- */
+ * PROTÓTIPOS DE FUNÇÃO;
+ *
+ ** Prototipo de Função; Pega e Retorna Valores; */
 function getValue(element) {
   return element.value;
 }
 
-/**
- * Prototipo de Função;
- * Seleciona todos os elementos;
- */
+/** Prototipo de Função; Seleciona todos os elementos; */
  function getElements(element) {
   return document.querySelectorAll(element);
 }
@@ -33,29 +30,50 @@ window.onload = () =>  {
    * Adiciona o valor do INPUT na lista; 
    */
   document.getElementById('criar-tarefa').addEventListener('click', () => {
-    setList(document.getElementById('texto-tarefa').value);
+    const text = document.getElementById('texto-tarefa');
+    if (text.value !== '' && text.value !== ' ') {
+      setList(text.value);
+      text.value = '';
+    }
   });
 
   /**
    * Adiciona o atributo selected quando o alvo for clicado;
    */
-  document.getElementById('lista-tarefas').addEventListener('click', event => {
+   document.getElementById('lista-tarefas').addEventListener('click', event => {
+     /* Pega todos os elementos da lista; */
+    const elements = document.getElementById('lista-tarefas').childNodes;
      /* Delegação de eventos; */
     const target = event.target;
-    /* Pega todos os elementos da lista; */
-    const elements = document.getElementById('lista-tarefas').childNodes;
+    
     /* Verifica se o elemento ja possúi o atributo selected; */
-    if (!event.target.hasAttribute('selected')) {
+    if (!target.hasAttribute('selected')) {
       /* Laço responsável por remover os atributos selected de todos os elementos da lista; */
       for (let value of elements) { // Percorre todos os elementos;
-        if (value.tagName == "LI") { // Verifica se o elemento é do tipo LI;
-          value.removeAttribute("selected"); // Remove o atributo selected do elemento;
+        if (value.tagName == 'LI') { // Verifica se o elemento é do tipo LI;
+          value.removeAttribute('selected'); // Remove o atributo selected do elemento;
+          value.removeAttribute('style');
         }
       }
 
       /* IF responsável por verificar se o elemento clicado é uma tag LI; */
       if (target.tagName == "LI" && target.nodeName == "LI") { // Verifica se o elemento clicado foi uma LI;
         event.target.setAttribute('selected', ''); // Adiciona o atributo selected ao elemento;
+        event.target.style.background = "rgb(128, 128, 128)";
+      }
+    }
+  });
+
+  /**
+   * Adiciona a classe completed quando o elemento for duplamente clicado;
+   */
+  document.getElementById('lista-tarefas').addEventListener('dblclick', event => {
+    const target = event.target;
+    if (target.tagName == 'LI' && target.nodeName == 'LI') {
+      if (!target.classList.contains('completed')) {
+        target.classList.add('completed');
+      } else {
+        target.classList.remove('completed');
       }
     }
   });
@@ -64,12 +82,32 @@ window.onload = () =>  {
    * Remove o elemento selecionado da lista;
    */
   document.getElementById('remove-task').addEventListener('click', () => {
+    /* Pega todos os elementos da lista; */
     const elements = document.getElementById('lista-tarefas').childNodes;
     for (let element of elements) {
-        if (element.tagName === "LI" && element.hasAttribute('selected')) {
+        if (element.tagName === 'LI' && element.hasAttribute('selected')) {
           element.remove();
-          alert('removed');
         }
+    }
+  });
+
+  /** Prototipo de Função; Remove todos os elementos da lista; */
+  document.getElementById('apaga-tudo').addEventListener('click', () => {
+    const main = document.getElementById('lista-tarefas');
+    for (let i = (main.childNodes.length - 1); i > 0; i--) {
+      if (main.childNodes[i].tagName == 'LI' && main.childNodes[i].nodeName == 'LI') {
+        main.childNodes[i].remove();
+      }
+    }
+  });
+
+  document.getElementById('remover-finalizados').addEventListener('click', () => {
+    const main = document.getElementById('lista-tarefas');
+    for (let i = (main.childNodes.length -1); i > 0; i--) {
+      if (main.childNodes[i].tagName == 'LI' && main.childNodes[i].nodeName == 'LI' && main.childNodes[i].classList.contains('completed')) {
+        main.childNodes[i].remove();
+
+      }
     }
   });
 }
