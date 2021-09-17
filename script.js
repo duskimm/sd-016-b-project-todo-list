7d2ca906e93e149190b66c400a0db1457e62b67b
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-16 23:34:57 -0300

    ajustes no lynt

--- script.js
+++ script.js
@@ -2,33 +2,30 @@ const addTaskButton = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const text = document.getElementById('texto-tarefa');
 
-addTaskButton.addEventListener('click', addTasks);
-function addTasks (){
-    const newListItem = document.createElement('li');
-    newListItem.addEventListener('click', changeToGrey);
-    list.appendChild(newListItem);
-    newListItem.innerText = text.value;
-    text.value = '';
+function addTasks() {
+  const newListItem = document.createElement('li');
+  newListItem.addEventListener('click', changeToGrey);
+  list.appendChild(newListItem);
+  newListItem.innerText = text.value;
+  text.value = '';
 }
+addTaskButton.addEventListener('click', addTasks);
 
 function changeToGrey (event){
-    const listItems = document.querySelectorAll('li');
-    for (let index = 0; index < listItems.length; index += 1) {
-        if (listItems[index].style.backgroundColor = 'rgb(128,128,128)') {
-            listItems[index].style.backgroundColor = 'white';
-        }
-    }
-    event.target.style.backgroundColor = 'rgb(128,128,128)'
-    }
+  const listItems = document.querySelectorAll('li');
+  for (let index = 0; index < listItems.length; index += 1) {
+      if (listItems[index].style.backgroundColor = 'rgb(128,128,128)') {
+          listItems[index].style.backgroundColor = 'white';
+      }
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)'
+  }
 
 list.addEventListener('dblclick', lineThrough);
 function lineThrough (event){
-    if (event.target.classList.contains('completed')) {
-        event.target.classList.remove('completed');
-    } else {
-        event.target.classList.add('completed');
-    }
+  if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+  } else {
+      event.target.classList.add('completed');
+  }
 }
-
-
-
