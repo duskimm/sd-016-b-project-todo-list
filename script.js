73a31a07782125528e12fab0d1c002715633a411
ErickVianaC <erickcosta535@gmail.com>
2021-09-17 21:15:28 -0300

    r6 feito

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function adicionar(){
-  const value = document.getElementById('texto-tarefa').value;
-  const list = document.getElementById('lista-tarefas').innerHTML;
+const button = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const text = document.getElementById('texto-tarefa');
 
+button.addEventListener('click', function (){
+  if(text.value.length > 0){
+  let item = document.createElement('li')
+  item.innerText = text.value;
+  list.appendChild(item);
+  text.value = null
 }
+})
