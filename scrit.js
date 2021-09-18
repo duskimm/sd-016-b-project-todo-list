f49500e135f8d4b44ed36316c388bc898b8b3f6e
Matheus Machado <matheusmdec@hotmail.com>
2021-09-17 21:47:50 -0300

    Requisito 09

--- scrit.js
+++ scrit.js
@@ -13,25 +13,71 @@ function addTaskToList() {
     list.appendChild(task)
     input.value = ''
     task.addEventListener('click', selectTask)
+    task.addEventListener('dblclick', completeTask)
 }
 
 //Requisito 07
 function selectTask(event) {
     let task = event.target
-    let taskList = document.querySelectorAll('.task')
-    console.log(task);
-    console.log(taskList);
-    for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].innerText !== task.innerText){
-            taskList[i].className = 'task'
-        } else {
-            taskList[i].className = 'task selected'
-        }
-        
+    let taskList = document.querySelectorAll('.task')    
+    if (alreadySelected(task)) {
+        task.classList.remove('selected')
+    } else {
+        for (let j = 0; j < taskList.length; j++) {
+            if (taskList[j].innerText !== task.innerText){
+                taskList[j].classList.remove('selected')
+            }else{
+                taskList[j].classList.add('selected')           
+            }        
+        }   
     }
-}   
+         
+}
+function alreadySelected(task) {
+    let taskSelected = document.querySelector('.selected')
+    if (taskSelected == null) {
+        taskSelected = []
+    } else {
+        if (task.innerText === taskSelected.innerText) {
+            return true
+        } else {
+            return false
+        } 
+    }
+    
+}
+    
 
 
+//Requisito 09
+function completeTask(event) {
+    let task = event.target
+    let taskList = document.querySelectorAll('.task')
+    if (alreadyCompleted(task)) {
+        task.classList.remove('completed')
+    } else {
+        for (let i = 0; i < taskList.length; i++) {
+            if (task.innerText === taskList[i].innerText) {
+                taskList[i].classList.add('completed')
+                console.log(true);
+            } else {
+                console.log(false);
+            }
+        }
+    }
+    
+}
+function alreadyCompleted(task) {
+    let taskList = document.querySelectorAll('.completed')
+    for (let i = 0; i < taskList.length; i++) {
+        if (task.innerText === taskList[i].innerText) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+   
 
 
 window.onload = function(){
