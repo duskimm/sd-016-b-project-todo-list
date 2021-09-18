5a0ddf5ef066f7c0f67f55f18b5be86423edd0e5
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-17 23:03:57 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,18 +1,64 @@
 window.onload = function() {
- 
-function listaTarefas(){
-    let btnTarefa = document.getElementById('criar-tarefa')
-    btnTarefa.addEventListener('click',function() {
-        let input = document.getElementById('texto-tarefa')
-        let listaTarefas = document.querySelector('.conteiner-tarefas #lista-tarefas')
-        let criaTarefa = document.createElement('li')
-        criaTarefa.innerHTML = input.value + '<br>'
-        listaTarefas.appendChild(criaTarefa)
-        input.value = ''
-    
-    })
 
-    
+function apagaTudo() {
+    let List = document.getElementById('tarefas')
+    if(List.parentNode){
+        List.parentNode.removeChild(List)
+    }
+}    
+
+function btnApagaTudo() {
+    let btnApagaTudo = document.getElementById('apaga-tudo')
+    btnApagaTudo.addEventListener('click',apagaTudo)
+}
+
+btnApagaTudo()
+
+function completaTafefa() {
+  let itensList = document.getElementsByClassName('colorTarefa')
+  for(let i = 0; i < itensList.length; i++) {
+    itensList[i].addEventListener('dbclick',function(event){
+          if(event.target.classList.contains('completed')){
+             event.target.classList.remove('completed')
+          }else{
+             event.target.classList.add('completed') 
+          }
+    })
+  }
+}    
+ 
+function pintaTarefa() {
+  let itensList = document.getElementsByClassName('colorTarefa')
+  
+  for(let i = 0; i < itensList.length; i++) {
+    itensList[i].addEventListener('click',function(event){
+          let selectedClass = document.getElementsByClassName('selected')
+          if(selectedClass.length == 0){
+             event.target.classList.add('selected')
+          }else{
+             for(let i = 0; i < itensList.length; i++ ) {
+               itensList[i].classList.remove('selected')
+             } 
+                event.target.classList.add('selected') 
+          }
+    })
+  }
+}
+
+function adicionaTarefa() {
+    let input = document.getElementById('texto-tarefa')
+    let listaTarefas = document.getElementById('lista-tarefas')
+    let criaTarefa = document.createElement('li')
+    criaTarefa.innerHTML = input.value + '<br>'
+    criaTarefa.className = "colorTarefa"
+    criaTarefa.addEventListener('click',pintaTarefa)
+    listaTarefas.appendChild(criaTarefa)
+    input.value = ''
+}
+
+function listaTarefas(){    
+    let btnTarefa = document.getElementById('criar-tarefa')
+    btnTarefa.addEventListener('click',adicionaTarefa)  
 }
     listaTarefas()
 }
